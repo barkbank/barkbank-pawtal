8a4a29fb78dbfcf61ceb79c10a4cfaaca539048b
Jin <hsienjin@gmail.com>
2024-02-17 13:42:13 +0800

    EmailOTP - Add a link to the login page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 "use server";
 
+import { BarkH1, BarkP } from "@/components/bark/bark-typography";
+import { Button } from "@/components/ui/button";
+import { RoutePath } from "@/lib/routes";
+import Link from "next/link";
+
 export default async function Page() {
   return (
     <>
-      <p>pre-login area</p>
+      <BarkH1>Bark Bank</BarkH1>
+      <BarkP>This is a public area.</BarkP>
+      <Link href={RoutePath.LOGIN_PAGE}><Button>Login</Button></Link>
     </>
   );
 }
