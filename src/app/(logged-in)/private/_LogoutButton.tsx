a55b451605d17d7ed1cf7b38ee11c7ebdf9f6661
Jin <hsienjin@gmail.com>
2024-02-17 11:11:35 +0800

    EmailOTP - Prettier

--- src/app/(logged-in)/private/_LogoutButton.tsx
+++ src/app/(logged-in)/private/_LogoutButton.tsx
@@ -1,11 +1,15 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 
-export function LogoutButton(props: {children: React.ReactNode}) {
+export function LogoutButton(props: { children: React.ReactNode }) {
   const onClick = async () => {
     signOut();
-  }
-  return (<><Button onClick={onClick}>{props.children}</Button></>)
+  };
+  return (
+    <>
+      <Button onClick={onClick}>{props.children}</Button>
+    </>
+  );
 }
