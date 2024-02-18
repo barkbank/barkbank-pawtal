d66a3618b8cd05be8f457cdb4600ea02c96183c2
Jin <hsienjin@gmail.com>
2024-02-18 17:53:00 +0800

    EmailOTP - Remove unused imports

--- src/app/(logged-in)/private/page.tsx
+++ src/app/(logged-in)/private/page.tsx
@@ -1,10 +1,5 @@
 "use server";
 
-import { getAuthenticatedAccount } from "@/lib/auth";
-import { RoutePath } from "@/lib/routes";
-import { redirect } from "next/navigation";
-import { LogoutLink } from "../_logout";
-
 export default async function Page() {
   return (
     <>
