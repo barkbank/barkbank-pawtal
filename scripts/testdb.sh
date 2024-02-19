e3253e2133c3cfdd09be0e158925c39b13a065a3
Jin <hsienjin@gmail.com>
2024-02-20 07:19:46 +0800

    DbSchema - Specify a named docker volume in testDbUp so we can remove it in testDbDown

--- scripts/testdb.sh
+++ scripts/testdb.sh
@@ -4,10 +4,12 @@ set -e
 
 mDockerImage=postgres:15.2
 mContainerName=pawtaltestdb
+mVolumeName=pawtaltestvolume
 
 function testDbDown {
     docker stop ${mContainerName} 2> /dev/null || :
     docker remove ${mContainerName} 2> /dev/null || :
+    docker volume rm ${mVolumeName} 2> /dev/null || :
 }
 
 function testDbUp {
@@ -16,6 +18,7 @@ function testDbUp {
            --name ${mContainerName} \
            -e POSTGRES_PASSWORD=password \
            -p 5999:5432 \
+           -v ${mVolumeName}:/var/lib/postgresql/data \
            -d \
            $mDockerImage
     sleep 3
