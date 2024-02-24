18cc6c0f951affd7444dacb9aa09085afe17e781
Jin <hsienjin@gmail.com>
2024-02-24 21:00:33 +0800

    Crypt - Implement encryption and hash services, update otp service

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -6,16 +6,38 @@ import {
   PassthroughEmailSender,
   SmtpConfig,
 } from "./services/email";
+import {
+  EncryptionService,
+  SecretEncryptionService,
+} from "./services/encryption";
+import { HashService, SecretHashService } from "./services/hash";
 import { OtpConfig, OtpService } from "./services/otp";
 import { guaranteed } from "./stringutils";
 
 class AppFactory {
   private emailService: EmailService | null = null;
   private otpService: OtpService | null = null;
+  private piiHashService: HashService | null = null;
+  private piiEncryptionService: EncryptionService | null = null;
 
   public async getEmailService(): Promise<EmailService> {
+    function resolveEmailSender(): EmailSender {
+      if (guaranteed(process.env.SMTP_HOST) === "") {
+        console.log("Using PassthroughEmailSender");
+        return new PassthroughEmailSender();
+      }
+      const config: SmtpConfig = {
+        smtpHost: guaranteed(process.env.SMTP_HOST),
+        smtpPort: parseInt(guaranteed(process.env.SMTP_PORT)),
+        smtpUser: guaranteed(process.env.SMTP_USER),
+        smtpPassword: guaranteed(process.env.SMTP_PASSWORD),
+      };
+      console.log("Using NodemailerEmailSender");
+      return new NodemailerEmailSender(config);
+    }
+
     if (!this.emailService) {
-      this.emailService = new EmailService(this.getEmailSender());
+      this.emailService = new EmailService(resolveEmailSender());
       console.log("Created EmailService");
     }
     return this.emailService;
@@ -29,9 +51,12 @@ class AppFactory {
         otpRecentPeriods: parseInt(
           guaranteed(process.env.OTP_NUM_RECENT_PERIODS),
         ),
-        otpServerSecret: guaranteed(process.env.OTP_SECRET),
+        otpHashService: new SecretHashService(
+          guaranteed(process.env.OTP_SECRET),
+        ),
       };
       this.otpService = new OtpService(config);
+      console.log("Created OtpService");
     }
     return this.otpService;
   }
@@ -43,19 +68,22 @@ class AppFactory {
     };
   }
 
-  private getEmailSender(): EmailSender {
-    if (guaranteed(process.env.SMTP_HOST) === "") {
-      console.log("Using PassthroughEmailSender");
-      return new PassthroughEmailSender();
+  public getPiiHashService(): HashService {
+    if (!this.piiHashService) {
+      this.piiHashService = new SecretHashService(
+        guaranteed(process.env.PII_SECRET),
+      );
     }
-    const config: SmtpConfig = {
-      smtpHost: guaranteed(process.env.SMTP_HOST),
-      smtpPort: parseInt(guaranteed(process.env.SMTP_PORT)),
-      smtpUser: guaranteed(process.env.SMTP_USER),
-      smtpPassword: guaranteed(process.env.SMTP_PASSWORD),
-    };
-    console.log("Using NodemailerEmailSender");
-    return new NodemailerEmailSender(config);
+    return this.piiHashService;
+  }
+
+  public getPiiEncryptionService(): EncryptionService {
+    if (!this.piiEncryptionService) {
+      this.piiEncryptionService = new SecretEncryptionService(
+        guaranteed(process.env.PII_SECRET),
+      );
+    }
+    return this.piiEncryptionService;
   }
 }
 
