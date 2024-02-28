b7a62f335561ffae6d0419e23bda619558498cb8
Jin <hsienjin@gmail.com>
2024-02-28 23:54:04 +0800

    Login3 - Impl getUserActor in UserActorFactory

--- src/lib/user/user-actor-factory.ts
+++ src/lib/user/user-actor-factory.ts
@@ -2,6 +2,7 @@ import { Pool } from "pg";
 import { EncryptionService } from "../services/encryption";
 import { HashService } from "../services/hash";
 import { UserActor, UserActorConfig } from "./user-actor";
+import { dbSelectUserByHashedEmail } from "../data/dbUsers";
 
 export type UserActorFactoryConfig = {
   dbPool: Pool;
@@ -17,7 +18,17 @@ export class UserActorFactory {
   }
 
   public async getUserActor(userEmail: string): Promise<UserActor | null> {
-    // WIP: Implement getUserActor
-    return null;
+    const { dbPool, emailHashService, piiEncryptionService } = this.config;
+    const userHashedEmail = await emailHashService.getHashHex(userEmail);
+    const user = await dbSelectUserByHashedEmail(dbPool, userHashedEmail);
+    if (user === null) {
+      return null;
+    }
+    const config: UserActorConfig = {
+      dbPool,
+      piiEncryptionService,
+    };
+    const actor = new UserActor(user.userId, config);
+    return actor;
   }
 }
