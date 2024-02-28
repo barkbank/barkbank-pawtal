dac60aee5026b77b7727fe5b6421575e3125a33a
Jin <hsienjin@gmail.com>
2024-02-29 01:06:11 +0800

    Login3 - Impl dangerous APIs

---
+++ src/app/api/dangerous/admins/route.ts
@@ -0,0 +1,18 @@
+import { AdminPii, encryptAdminPii } from "@/lib/admin/admin-pii";
+import APP from "@/lib/app";
+import { dbInsertAdmin } from "@/lib/data/dbAdmins";
+import { AdminSpec } from "@/lib/data/models";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const body = await request.json();
+  const pii = body as AdminPii;
+  const emailHashService = await APP.getEmailHashService();
+  const piiEncryptionService = await APP.getPiiEncryptionService();
+  const adminHashedEmail = await emailHashService.getHashHex(pii.adminEmail);
+  const adminEncryptedPii = await encryptAdminPii(pii, piiEncryptionService);
+  const spec: AdminSpec = {adminHashedEmail, adminEncryptedPii}
+  const dbPool = await APP.getDbPool();
+  const gen = await dbInsertAdmin(dbPool, spec);
+  return NextResponse.json(gen);
+}
---
+++ src/app/api/dangerous/users/route.ts
@@ -0,0 +1,18 @@
+import { UserPii, encryptUserPii } from "@/lib/user/user-pii";
+import APP from "@/lib/app";
+import { dbInsertUser } from "@/lib/data/dbUsers";
+import { UserSpec } from "@/lib/data/models";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const body = await request.json();
+  const pii = body as UserPii;
+  const emailHashService = await APP.getEmailHashService();
+  const piiEncryptionService = await APP.getPiiEncryptionService();
+  const userHashedEmail = await emailHashService.getHashHex(pii.userEmail);
+  const userEncryptedPii = await encryptUserPii(pii, piiEncryptionService);
+  const spec: UserSpec = {userHashedEmail, userEncryptedPii}
+  const dbPool = await APP.getDbPool();
+  const gen = await dbInsertUser(dbPool, spec);
+  return NextResponse.json(gen);
+}
