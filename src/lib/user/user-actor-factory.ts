ffbc029b31567ad4cc3b6cbfd9d2ac4fb38149a4
Jin <hsienjin@gmail.com>
2024-02-28 09:19:58 +0800

    Login3 - Stub UserActorFactory

---
+++ src/lib/user/user-actor-factory.ts
@@ -0,0 +1,20 @@
+import { EncryptionService } from "../services/encryption";
+import { HashService } from "../services/hash";
+import { UserActor, UserActorConfig } from "./user-actor";
+
+export type UserActorFactoryConfig = {  
+  emailHashService: HashService;
+  piiEncryptionService: EncryptionService;
+}
+
+export class UserActorFactory {
+  private config: UserActorFactoryConfig;
+
+  constructor(config: UserActorFactoryConfig) {
+    this.config = config;
+  }
+
+  public async getUserActor(userEmail: string): Promise<UserActor | null> {
+    return null;
+  }
+}
--- src/lib/user/user-actor.ts
+++ src/lib/user/user-actor.ts
@@ -1,8 +1,6 @@
 import { EncryptionService } from "../services/encryption";
 
 export type UserActorConfig = {
-  userId: string;
-
   /**
    * For user to decrypt their own PII
    */
@@ -13,13 +11,15 @@ export type UserActorConfig = {
  * Actor for user accounts
  */
 export class UserActor {
+  private userId: string;
   private config: UserActorConfig;
 
-  constructor(config: UserActorConfig) {
+  constructor(userId: string, config: UserActorConfig) {
+    this.userId = userId;
     this.config = config;
   }
 
   public getUserId(): string {
-    return this.config.userId;
+    return this.userId;
   }
 }
