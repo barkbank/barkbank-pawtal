d10a76d9919ac2965ae7408ddc5937f6933f7050
Jin <hsienjin@gmail.com>
2024-02-29 00:08:34 +0800

    Login3 - Use UserActor in demo User Dashboard

--- src/app/user/(logged-in)/dashboard/page.tsx
+++ src/app/user/(logged-in)/dashboard/page.tsx
@@ -1,11 +1,27 @@
 "use server";
 
+import { BarkH1 } from "@/components/bark/bark-typography";
+import { getAuthenticatedUserActor } from "@/lib/auth";
+import { RoutePath } from "@/lib/route-path";
+import { UserPii } from "@/lib/user/user-pii";
+import { redirect } from "next/navigation";
+
 export default async function Page() {
-  // WIP: Use getAuthenticatedUserActor here
-  // WIP: Demonstrate retrieval of PII
+  const actor = await getAuthenticatedUserActor();
+  if (!actor) {
+    redirect(RoutePath.USER_LOGIN_PAGE);
+  }
+  const ownPii: UserPii | null = await actor.getOwnPii();
+  if (!ownPii) {
+    redirect(RoutePath.USER_LOGIN_PAGE);
+  }
   return (
     <>
-      <p>User Dashboard</p>
+      <BarkH1>User Dashboard</BarkH1>
+      <div>
+        My Own PII
+        <pre>{JSON.stringify(ownPii, null, 2)}</pre>
+      </div>
     </>
   );
 }
