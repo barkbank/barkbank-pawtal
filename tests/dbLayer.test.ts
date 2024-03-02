7a9f3835e2bf3d6005f5e01c0a58d742279c7566
Jin <hsienjin@gmail.com>
2024-03-02 09:59:42 +0800

    RefactorDbLayerTests - Refactor tests for dbVets

--- tests/dbLayer.test.ts
+++ tests/dbLayer.test.ts
@@ -259,35 +259,49 @@ describe("Database Layer", () => {
     });
   });
   describe("dbVets", () => {
-    it("should support insert and select", async () => {
-      await withDb(async (db) => {
-        const vetGen = await dbInsertVet(db, vetSpec(1));
-        const vet = await dbSelectVet(db, vetGen.vetId);
-        if (!vet) fail("vet is null");
-        expect(vet.vetCreationTime).toBeTruthy();
-        expect(vet.vetModificationTime).toBeTruthy();
-        expect(vet.vetModificationTime).toEqual(vet.vetCreationTime);
-        const spec = toVetSpec(vet);
-        expect(spec).toMatchObject(vetSpec(1));
+    describe("dbInsertVet", () => {
+      it("should return VetGen", async () => {
+        await withDb(async (db) => {
+          const vetGen = await dbInsertVet(db, vetSpec(1));
+          expect(vetGen.vetCreationTime).toBeTruthy();
+          expect(vetGen.vetModificationTime).toBeTruthy();
+          expect(vetGen.vetModificationTime).toEqual(vetGen.vetCreationTime);
+        });
       });
     });
-    it("should return null when vet does not exist", async () => {
-      await withDb(async (db) => {
-        const vet = await dbSelectVet(db, "111");
-        expect(vet).toBeNull();
+    describe("dbSelectVet", () => {
+      it("should return vet matching the vetId", async () => {
+        await withDb(async (db) => {
+          const vetGen = await dbInsertVet(db, vetSpec(1));
+          const vet = await dbSelectVet(db, vetGen.vetId);
+          expect(vet).not.toBeNull();
+          expect(vet?.vetCreationTime).toBeTruthy();
+          expect(vet?.vetModificationTime).toBeTruthy();
+          expect(vet?.vetModificationTime).toEqual(vet?.vetCreationTime);
+          const spec = toVetSpec(guaranteed(vet));
+          expect(spec).toMatchObject(vetSpec(1));
+        });
+      });
+      it("should return null no vet matches the vetId", async () => {
+        await withDb(async (db) => {
+          const vet = await dbSelectVet(db, "111");
+          expect(vet).toBeNull();
+        });
       });
     });
-    it("should support retrieval of vet ID by email", async () => {
-      await withDb(async (db) => {
-        const vetGen = await dbInsertVet(db, vetSpec(1));
-        const vetId = await dbSelectVetIdByEmail(db, vetSpec(1).vetEmail);
-        expect(vetId).toEqual(vetGen.vetId);
+    describe("dbSelectVetIdByEmail", () => {
+      it("should return vetId matching the email", async () => {
+        await withDb(async (db) => {
+          const vetGen = await dbInsertVet(db, vetSpec(1));
+          const vetId = await dbSelectVetIdByEmail(db, vetSpec(1).vetEmail);
+          expect(vetId).toEqual(vetGen.vetId);
+        });
       });
-    });
-    it("should return null when no vet exists with the email", async () => {
-      await withDb(async (db) => {
-        const vetId = await dbSelectVetIdByEmail(db, "notavet@vet.com");
-        expect(vetId).toBeNull();
+      it("should return null when no vet matches the email", async () => {
+        await withDb(async (db) => {
+          const vetId = await dbSelectVetIdByEmail(db, "notavet@vet.com");
+          expect(vetId).toBeNull();
+        });
       });
     });
   });
