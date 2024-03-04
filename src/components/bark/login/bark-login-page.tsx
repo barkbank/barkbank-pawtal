86aac9188a2aafb4f553d7e8bdcffaaf1e11b2ef
Brennan <tanjialebrennan@gmail.com>
2024-03-04 21:47:23 +0800

    feat: update H2 typography

--- src/components/bark/login/bark-login-page.tsx
+++ src/components/bark/login/bark-login-page.tsx
@@ -2,7 +2,7 @@
 
 import { isLoggedIn } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { BarkH1 } from "@/components/bark/bark-typography";
+import { BarkH2 } from "@/components/bark/bark-typography";
 import { AccountType } from "@/lib/auth";
 import BarkLoginForm from "./bark-login-form";
 import LoginLayout from "./layout";
@@ -22,7 +22,7 @@ export default async function BarkLoginPage(props: {
       <div className="flex h-screen flex-col items-center justify-center">
         <LoginLayout logoSrc={logoSrc}>
           <div className="mx-auto w-full max-w-[1100px] sm:w-[36rem] md:w-full">
-            <BarkH1>{title}</BarkH1>
+            <BarkH2>{title}</BarkH2>
             <BarkLoginForm
               accountType={accountType}
               successPath={successPath}
