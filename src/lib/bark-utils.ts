066871d7c53a9143de43c533d66f19201d78a46e
Jin <hsienjin@gmail.com>
2024-03-05 08:31:00 +0800

    Root - Provide the value of BARKBANK_ROOT_ADMIN_EMAIL to the admin actor factory

--- src/lib/bark-utils.ts
+++ src/lib/bark-utils.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export function guaranteed<T>(arg: T | undefined | null): T {
   if (arg === undefined) {
     throw Error("arg is undefined");
@@ -6,4 +8,13 @@ export function guaranteed<T>(arg: T | undefined | null): T {
     throw Error("arg is null");
   }
   return arg;
+}
+
+export function isValidEmail(email: string): boolean {
+  try {
+    z.string().email().parse(email);
+    return true;
+  } catch {
+    return false;
+  }
 }
