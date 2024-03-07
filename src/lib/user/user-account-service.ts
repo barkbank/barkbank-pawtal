7fde02ce0861b2436f643c9fe71af838049906fc
Jin <hsienjin@gmail.com>
2024-03-08 00:37:14 +0800

    CreateUserAccount - Impl createUserAccount

--- src/lib/user/user-account-service.ts
+++ src/lib/user/user-account-service.ts
@@ -1,10 +1,19 @@
 import { Pool } from "pg";
 import { HashService } from "../services/hash";
 import { EncryptionService } from "../services/encryption";
-import { Dog, User } from "../data/db-models";
-import { dbSelectUser, dbSelectUserIdByHashedEmail } from "../data/db-users";
-import { Registration, UserPii } from "./user-models";
-import { dbSelectDogListByUserId } from "../data/db-dogs";
+import { Dog, DogStatus, User } from "../data/db-models";
+import {
+  dbSelectUser,
+  dbSelectUserIdByHashedEmail,
+  dbTryInsertUser,
+} from "../data/db-users";
+import { Registration, UserPii, encryptDogOii } from "./user-models";
+import {
+  dbInsertDog,
+  dbInsertDogVetPreference,
+  dbSelectDogListByUserId,
+} from "../data/db-dogs";
+import { dbBegin, dbCommit, dbRelease, dbRollback } from "../data/db-utils";
 
 export type UserAccountServiceConfig = {
   dbPool: Pool;
@@ -43,8 +52,64 @@ export class UserAccountService {
     return this.decryptUserPii(user.userEncryptedPii);
   }
 
-  public createUserAccount(registration: Registration): Promise<boolean> {
-    return Promise.resolve(true);
+  public async createUserAccount(registration: Registration): Promise<boolean> {
+    const conn = await this.getDbPool().connect();
+    try {
+      await dbBegin(conn);
+      const { userEmail, userName, userPhoneNumber } = registration.user;
+      const [userHashedEmail, userEncryptedPii] = await Promise.all([
+        this.getEmailHashService().getHashHex(userEmail),
+        this.encryptUserPii({
+          userEmail,
+          userName,
+          userPhoneNumber,
+        }),
+      ]);
+      const userSpec = { userHashedEmail, userEncryptedPii };
+      const userGen = await dbTryInsertUser(conn, userSpec);
+      if (userGen === null) {
+        await dbRollback(conn);
+        return false;
+      }
+      for (const dogRegistration of registration.dogList) {
+        const {
+          dogName,
+          dogBreed,
+          dogBirthday,
+          dogGender,
+          dogWeightKg,
+          dogDea1Point1,
+          dogEverPregnant,
+          dogEverReceivedTransfusion,
+          dogPreferredVetIdList,
+        } = dogRegistration;
+        const dogEncryptedOii = await encryptDogOii(
+          { dogName },
+          this.getPiiEncryptionService(),
+        );
+        const dogGen = await dbInsertDog(conn, userGen.userId, {
+          dogBreed,
+          dogBirthday,
+          dogGender,
+          dogWeightKg,
+          dogDea1Point1,
+          dogEverPregnant,
+          dogEverReceivedTransfusion,
+          dogEncryptedOii,
+          dogStatus: DogStatus.NEW_PROFILE,
+        });
+        for (const vetId of dogPreferredVetIdList) {
+          await dbInsertDogVetPreference(conn, dogGen.dogId, vetId);
+        }
+      }
+      await dbCommit(conn);
+      return true;
+    } catch {
+      await dbRollback(conn);
+      return false;
+    } finally {
+      await dbRelease(conn);
+    }
   }
 
   public getUserDogs(userId: string): Promise<Dog[]> {
