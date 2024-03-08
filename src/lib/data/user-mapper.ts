12f64dfd9f185d39df1cf848f4925a01e62771ee
Jin <hsienjin@gmail.com>
2024-03-08 20:56:09 +0800

    ReviseModels - Impl other methods in UserMapper

--- src/lib/data/user-mapper.ts
+++ src/lib/data/user-mapper.ts
@@ -1,6 +1,6 @@
 import { EncryptionService } from "../services/encryption";
 import { HashService } from "../services/hash";
-import { UserRecord, UserSpec } from "./db-models";
+import { User, UserGen, UserPii, UserRecord, UserSpec } from "./db-models";
 
 export class UserMapper {
   private emailHashService: HashService;
@@ -16,6 +16,35 @@ export class UserMapper {
 
   public mapUserRecordToUserSpec(userRecord: UserRecord): UserSpec {
     const { userHashedEmail, userEncryptedPii } = userRecord;
+    return { userHashedEmail, userEncryptedPii };
+  }
+
+  public mapUserRecordToUserGen(userRecord: UserRecord): UserGen {
+    const { userId, userCreationTime, userModificationTime } = userRecord;
+    return { userId, userCreationTime, userModificationTime };
+  }
+
+  public async mapUserRecordToUser(userRecord: UserRecord): Promise<User> {
+    const userGen = this.mapUserRecordToUserGen(userRecord);
+    const userSpec = this.mapUserRecordToUserSpec(userRecord);
+    const userPii = await this.mapUserSpecToUserPii(userSpec);
+    return { ...userGen, ...userPii };
+  }
+
+  public async mapUserSpecToUserPii(userSpec: UserSpec): Promise<UserPii> {
+    const jsonEncoded = await this.piiEncryptionService.getDecryptedData(
+      userSpec.userEncryptedPii,
+    );
+    const pii = JSON.parse(jsonEncoded) as UserPii;
+    return pii;
+  }
+
+  public async mapUserPiiToUserSpec(userPii: UserPii): Promise<UserSpec> {
+    const jsonEncoded = JSON.stringify(userPii);
+    const [userHashedEmail, userEncryptedPii] = await Promise.all([
+      this.emailHashService.getHashHex(userPii.userEmail),
+      this.piiEncryptionService.getEncryptedData(jsonEncoded),
+    ]);
     return { userHashedEmail, userEncryptedPii };
   }
 }
