ba81ada395f55555d7b97303ce32f4a29d9a246b
Jin <hsienjin@gmail.com>
2024-03-08 20:48:05 +0800

    ReviseModels - Delete db-mappers.ts

--- src/lib/data/db-mappers.ts
+++
@@ -1,6 +0,0 @@
-import { UserRecord, UserSpec } from "./db-models";
-
-export function toUserSpec(user: UserRecord): UserSpec {
-  const { userId, userCreationTime, userModificationTime, ...spec } = user;
-  return spec;
-}
