6c90f25d287abf8373fb9862fd2eccaaf3a771fe
Brennan <tanjialebrennan@gmail.com>
2024-03-20 21:45:25 +0800

    fix: nav styling

--- src/app/_components/root-header.tsx
+++ src/app/_components/root-header.tsx
@@ -11,29 +11,27 @@ const RootHeader = async () => {
   const accountType = session?.accountType;
 
   return (
-    <nav className="sticky top-0 z-10 bg-white">
-      <div className=" flex items-center justify-between border-b shadow-lg">
-        <Link href="/">
-          <Image
-            src="/bark-bank-logo.svg"
-            alt="bark bank logo"
-            width={60}
-            height={60}
-            className="ml-4 md:ml-10"
-          />
-        </Link>
+    <nav className="sticky top-0 z-10 flex h-[72px] items-center justify-between border-b bg-white shadow-lg">
+      <Link href="/">
+        <Image
+          src="/bark-bank-logo.svg"
+          alt="bark bank logo"
+          width={60}
+          height={60}
+          className="ml-4 md:ml-10"
+        />
+      </Link>
 
-        <div className="hidden gap-x-4 sm:flex sm:last:mr-10">
-          <Link href="/">Home</Link>
-          <Link href={RoutePath.ABOUT_US}>About Us</Link>
-          <Link href={RoutePath.BE_A_DONOR}>Be a Donor</Link>
-          <Link href={RoutePath.ARTICLES}>Articles</Link>
-          <Link href={RoutePath.FAQ}>FAQ</Link>
-          <Link href={RoutePath.INFO}>Info</Link>
-          <Link href={`/${accountType?.toLowerCase()}/dashboard`}>
-            <CircleUser />
-          </Link>
-        </div>
+      <div className="hidden gap-x-8 sm:flex sm:last:mr-8">
+        <Link href="/">Home</Link>
+        <Link href={RoutePath.ABOUT_US}>About Us</Link>
+        <Link href={RoutePath.BE_A_DONOR}>Be a Donor</Link>
+        <Link href={RoutePath.ARTICLES}>Articles</Link>
+        <Link href={RoutePath.FAQ}>FAQ</Link>
+        <Link href={RoutePath.INFO}>Info</Link>
+        <Link href={`/${accountType?.toLowerCase()}/dashboard`}>
+          <CircleUser />
+        </Link>
       </div>
     </nav>
   );
