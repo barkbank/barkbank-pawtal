b6a7d0c6e3251fd02ae574c2bf2d260455cc40fd
Jin <hsienjin@gmail.com>
2024-03-24 16:36:33 +0800

    user-actor-my-pets - impl action for list-my-pets

---
+++ src/app/user/(logged-in)/my-pets/_actions/list-my-pets.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import APP from "@/lib/app";
+import { getAuthenticatedUserActor } from "@/lib/auth";
+import { ListMyPetsHandler, ListMyPetsResponse } from "@/lib/user/list-my-pets-handler";
+
+/**
+ * @returns ListMyPetsResponse if logged in, null if not logged in.
+ */
+export async function listMyPets(): Promise<ListMyPetsResponse | null> {
+  const actor = await getAuthenticatedUserActor();
+  if (actor === null) {
+    return null;
+  }
+  const handler = new ListMyPetsHandler({
+    dbPool: await APP.getDbPool(),
+    dogMapper: await APP.getDogMapper(),
+  })
+  return handler.handle(actor);
+}
