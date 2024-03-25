004f272458388b63210269b0c861b8a9879dd9f1
Jin <hsienjin@gmail.com>
2024-03-25 09:21:37 +0800

    user-actor-my-pets - move list-my-pets-handler into lib/handlers

--- src/lib/actions/user-get-my-pets.ts
+++ src/lib/actions/user-get-my-pets.ts
@@ -2,7 +2,7 @@
 
 import APP from "@/lib/app";
 import { getAuthenticatedUserActor } from "@/lib/auth";
-import { ListMyPetsHandler } from "@/lib/user/handlers/list-my-pets-handler";
+import { ListMyPetsHandler } from "@/lib/handlers/list-my-pets-handler";
 import { ListMyPetsResponse } from "../models/user-models";
 import { Err, Ok, Result } from "../result";
 import { ERR_401_NO_ACTOR, ERR_500_SERVER_ERROR } from "./action-errors";
