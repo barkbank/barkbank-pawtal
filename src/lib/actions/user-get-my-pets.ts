6ddaab6d075e189bf53f794bd760cc7efb1ad58c
Jin <hsienjin@gmail.com>
2024-03-25 09:09:40 +0800

    user-actor-my-pets - move list-my-pets into lib/actions/user-get-my-pets

---
+++ src/lib/actions/user-get-my-pets.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import APP from "@/lib/app";
+import { getAuthenticatedUserActor } from "@/lib/auth";
+import { ListMyPetsHandler } from "@/lib/user/handlers/list-my-pets-handler";
+import { ListMyPetsResponse } from "../user/user-models";
+import { Err, Ok, Result } from "../result";
+import { ERR_401_NO_ACTOR, ERR_500_SERVER_ERROR } from "../action-errors";
+
+export async function userGetMyPets(): Promise<
+  Result<
+    ListMyPetsResponse,
+    typeof ERR_401_NO_ACTOR | typeof ERR_500_SERVER_ERROR
+  >
+> {
+  const actor = await getAuthenticatedUserActor();
+  if (actor === null) {
+    return Err(ERR_401_NO_ACTOR);
+  }
+  try {
+    const handler = new ListMyPetsHandler({
+      dbPool: await APP.getDbPool(),
+      dogMapper: await APP.getDogMapper(),
+    });
+    const response = await handler.handle(actor);
+    return Ok(response);
+  } catch {
+    return Err(ERR_500_SERVER_ERROR);
+  }
+}
