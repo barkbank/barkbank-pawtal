d83388e3ff35982489d6d7e81da36fa28309b22a
Jin <hsienjin@gmail.com>
2024-03-30 15:33:21 +0800

    refactor - rename to RegistrationService

--- src/app/user/registration/_actions/register-new-user.ts
+++ src/app/user/registration/_actions/register-new-user.ts
@@ -4,12 +4,11 @@ import APP from "@/lib/app";
 import {
   RegistrationRequest,
   RegistrationResponse,
-} from "@/lib/services/registration-handler";
+} from "@/lib/services/registration";
 
 export async function registerNewUser(
   request: RegistrationRequest,
 ): Promise<RegistrationResponse> {
-  // WIP: refer to registration service
-  const handler = await APP.getRegistrationHandler();
-  return handler.handle(request);
+  const service = await APP.getRegistrationService();
+  return service.handle(request);
 }
