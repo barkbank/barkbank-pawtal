8113d54f717c081c2d19e56275f67e6c3ff63e24
Jin <hsienjin@gmail.com>
2024-04-03 21:58:45 +0800

    update-my-dog-details - test should return ERROR_MISSING_DOG dog not found

--- src/lib/user/actions/update-my-dog-details.ts
+++ src/lib/user/actions/update-my-dog-details.ts
@@ -1,5 +1,7 @@
+import { PoolClient } from "pg";
 import { UserActor } from "../user-actor";
 import { MyDogDetailsUpdate } from "../user-models";
+import { dbBegin, dbCommit, dbQuery, dbRelease, dbRollback } from "@/lib/data/db-utils";
 
 type Response =
   | "OK_UPDATED"
@@ -8,9 +10,46 @@ type Response =
   | "ERROR_MISSING_REPORT"
   | "FAILURE_DB_UPDATE";
 
+type Context = {
+  actor: UserActor;
+  update: MyDogDetailsUpdate;
+};
+
 export async function updateMyDogDetails(
   actor: UserActor,
   update: MyDogDetailsUpdate,
 ): Promise<Response> {
-  return "OK_UPDATED";
+  const ctx: Context = { actor, update };
+  const { dbPool } = actor.getParams();
+  const conn = await dbPool.connect();
+  try {
+    await dbBegin(conn);
+    const resOwnership = await checkOwnership(conn, ctx);
+    if (resOwnership !== "OK") {
+      return resOwnership;
+    }
+    await dbCommit(conn);
+    return "OK_UPDATED";
+  } finally {
+    await dbRollback(conn);
+    await dbRelease(conn);
+  }
+}
+
+async function checkOwnership(
+  conn: PoolClient,
+  ctx: Context,
+): Promise<"OK" | "ERROR_MISSING_DOG" | "ERROR_UNAUTHORIZED"> {
+  const { actor, update } = ctx;
+  const sql = `SELECT user_id as "ownerUserId" FROM dogs WHERE dog_id = $1`;
+  const res = await dbQuery(conn, sql, [update.dogId]);
+  if (res.rows.length === 0) {
+    return "ERROR_MISSING_DOG";
+  }
+  const { ownerUserId } = res.rows[0];
+  const isOwner = actor.getUserId() === ownerUserId;
+  if (!isOwner) {
+    return "ERROR_UNAUTHORIZED";
+  }
+  return "OK";
 }
--- tests/user/update-my-dog-details.test.ts
+++ tests/user/update-my-dog-details.test.ts
@@ -1,4 +1,8 @@
+import { MyDogDetailsUpdate } from "@/lib/user/user-models";
 import { withDb } from "../_db_helpers";
+import { PARTICIPATION_STATUS, YesNoUnknown } from "@/lib/data/db-enums";
+import { getUserActor, insertUser } from "../_fixtures";
+import { updateMyDogDetails } from "@/lib/user/actions/update-my-dog-details";
 
 describe("updateMyDogDetails", () => {
   it("should return OK_UPDATED when successfully updated dog details", async () => {
@@ -11,6 +15,31 @@ describe("updateMyDogDetails", () => {
     await withDb(async (dbPool) => {});
   });
   it("should return ERROR_MISSING_DOG dog not found", async () => {
-    await withDb(async (dbPool) => {});
+    await withDb(async (dbPool) => {
+      const u1 = await insertUser(1, dbPool);
+      const unknownDogId = "123";
+      const update = detailsUpdate(unknownDogId);
+      const actor = getUserActor(dbPool, u1.userId);
+      const res = await updateMyDogDetails(actor, update);
+      expect(res).toEqual("ERROR_MISSING_DOG");
+    });
   });
 });
+
+function detailsUpdate(
+  dogId: string,
+  overrides?: Partial<MyDogDetailsUpdate>,
+): MyDogDetailsUpdate {
+  const base: MyDogDetailsUpdate = {
+    dogId,
+    dogName: "updated name",
+    dogWeightKg: 50,
+    dogEverPregnant: YesNoUnknown.NO,
+    dogEverReceivedTransfusion: YesNoUnknown.NO,
+    dogPreferredVetId: null,
+    dogParticipationStatus: PARTICIPATION_STATUS.PARTICIPATING,
+    // TODO: dogPauseEndReason: string; When the schema supports it
+    dogPauseExpiryTime: null,
+  };
+  return { ...base, ...overrides };
+}
