f9deada6d35a8daf95877883507b7e366be12b50
Jin <hsienjin@gmail.com>
2024-04-06 15:24:19 +0800

    get-num-incomplete-profiles - implement get num incomplete profiles

--- src/lib/admin/actions/get-num-incomplete-profiles.ts
+++ src/lib/admin/actions/get-num-incomplete-profiles.ts
@@ -1,5 +1,6 @@
-import { Err, Result } from "@/lib/utilities/result";
+import { Err, Ok, Result } from "@/lib/utilities/result";
 import { AdminActor } from "../admin-actor";
+import { dbQuery } from "@/lib/data/db-utils";
 
 type ErrorCode = "ERROR_UNAUTHORIZED";
 
@@ -9,5 +10,18 @@ type ErrorCode = "ERROR_UNAUTHORIZED";
 export async function getNumIncompleteProfiles(
   actor: AdminActor,
 ): Promise<Result<number, ErrorCode>> {
-  return Err("ERROR_UNAUTHORIZED");
+  const { adminCanManageDonors } = await actor.getPermissions();
+  if (!adminCanManageDonors) {
+    return Err("ERROR_UNAUTHORIZED");
+  }
+  const { dbPool } = actor.getParams();
+  const sql = `
+  SELECT COUNT(1)::integer as "numProfiles"
+  FROM dog_statuses
+  WHERE profile_status = 'INCOMPLETE'
+  AND participation_status = 'PARTICIPATING'
+  `;
+  const res = await dbQuery(dbPool, sql, []);
+  const { numProfiles } = res.rows[0];
+  return Ok(numProfiles);
 }
