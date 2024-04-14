3c7eeac4a99c578cdf2d0865f66882c2b10f8f57
Jin <hsienjin@gmail.com>
2024-04-14 15:03:21 +0800

    add-user-last-contacted-time - stub the schedule-appointments page with json data

---
+++ src/app/vet/(logged-in)/schedule-appointments/page.tsx
@@ -0,0 +1,25 @@
+import { getAuthenticatedVetActor } from "@/lib/auth";
+import { RoutePath } from "@/lib/route-path";
+import { getAvailableDogs } from "@/lib/vet/actions/get-available-dogs";
+import { getOwnerContactDetails } from "@/lib/vet/actions/get-owner-contact-details";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const actor = await getAuthenticatedVetActor();
+  if (!actor) {
+    redirect(RoutePath.VET_LOGIN_PAGE);
+  }
+  const availableDogs = await getAvailableDogs(actor);
+  const data = await Promise.all(
+    availableDogs.map(async (availableDog) => {
+      const { dogId } = availableDog;
+      const ownerContactDetails = await getOwnerContactDetails(actor, dogId);
+      return { availableDog, ownerContactDetails };
+    }),
+  );
+  return (
+    <div>
+      Data: <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
