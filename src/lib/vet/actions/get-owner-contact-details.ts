4975b84e5855509113271532475c60152bc457b9
Jin <hsienjin@gmail.com>
2024-04-14 11:59:11 +0800

    get-owner-contact-details - pass all the tests

--- src/lib/vet/actions/get-owner-contact-details.ts
+++ src/lib/vet/actions/get-owner-contact-details.ts
@@ -1,6 +1,7 @@
-import { Err, Result } from "@/lib/utilities/result";
+import { Err, Ok, Result } from "@/lib/utilities/result";
 import { VetActor } from "../vet-actor";
 import { OwnerContactDetails } from "../vet-models";
+import { dbQuery } from "@/lib/data/db-utils";
 
 type ErrorCode = "ERROR_UNAUTHORIZED" | "ERROR_NO_DOG";
 
@@ -17,5 +18,60 @@ export async function getOwnerContactDetails(
   actor: VetActor,
   dogId: string,
 ): Promise<Result<OwnerContactDetails, ErrorCode>> {
-  return Err("ERROR_UNAUTHORIZED");
+  const ctx: Context = { actor, dogId };
+  const row: Row | null = await fetchRow(ctx);
+  if (row === null) {
+    return Err("ERROR_NO_DOG");
+  }
+  if (row.isPreferredVet === false) {
+    return Err("ERROR_UNAUTHORIZED");
+  }
+  const res = await toOwnerContactDetails(ctx, row);
+  return Ok(res);
+}
+
+type Context = {
+  actor: VetActor;
+  dogId: string;
+};
+
+type Row = {
+  userEncryptedPii: string;
+  isPreferredVet: boolean;
+};
+
+async function fetchRow(ctx: Context): Promise<Row | null> {
+  const { actor, dogId } = ctx;
+  const { dbPool, vetId } = actor.getParams();
+  const sql = `
+  SELECT
+    tUser.user_encrypted_pii as "userEncryptedPii",
+    $2 IN (
+      SELECT vet_id
+      FROM dog_vet_preferences
+      WHERE dog_id = $1
+    ) as "isPreferredVet"
+  FROM dogs as tDog
+  LEFT JOIN users as tUser on tDog.user_id = tUser.user_id
+  WHERE dog_id = $1
+  `;
+  const res = await dbQuery<Row>(dbPool, sql, [dogId, vetId]);
+  return res.rows.length === 1 ? res.rows[0] : null;
+}
+
+async function toOwnerContactDetails(
+  ctx: Context,
+  row: Row,
+): Promise<OwnerContactDetails> {
+  const { actor, dogId } = ctx;
+  const { userMapper } = actor.getParams();
+  const { userEncryptedPii } = row;
+  const { userName, userEmail, userPhoneNumber } =
+    await userMapper.mapUserEncryptedPiiToUserPii({ userEncryptedPii });
+  return {
+    dogId,
+    userName,
+    userEmail,
+    userPhoneNumber,
+  };
 }
