b21a19ff884e7414f30ef853b4519b81858831a5
Jin <hsienjin@gmail.com>
2024-04-15 09:12:13 +0800

    call-outcome - convert dbInsertReport into dbInsertReportAndUpdateCall

--- src/lib/data/db-reports.ts
+++ src/lib/data/db-reports.ts
@@ -1,39 +1,74 @@
+import { CallOutcome } from "./db-enums";
 import { DbReportGen, DbReportSpec } from "./db-models";
 import { DbContext, dbQuery } from "./db-utils";
 
-export async function dbInsertReport(
+export async function dbInsertReportAndUpdateCall(
   dbCtx: DbContext,
   reportSpec: DbReportSpec,
 ): Promise<DbReportGen> {
   const sql = `
-  INSERT INTO reports (
-    call_id,
-    dog_id,
-    vet_id,
-    visit_time,
-    dog_weight_kg,
-    dog_body_conditioning_score,
-    dog_did_donate_blood,
-    dog_heartworm,
-    dog_dea1_point1,
-    dog_reported_ineligibility,
-    encrypted_ineligibility_reason,
-    ineligibility_expiry_time
+  WITH
+  mCallDetails as (
+    SELECT call_id, dog_id, vet_id
+    FROM calls
+    WHERE call_id = $1
+    AND call_outcome = 'APPOINTMENT'
+  ),
+  mInsertReport as (
+    INSERT INTO reports (
+      call_id,
+      dog_id,
+      vet_id,
+      visit_time,
+      dog_weight_kg,
+      dog_body_conditioning_score,
+      dog_did_donate_blood,
+      dog_heartworm,
+      dog_dea1_point1,
+      dog_reported_ineligibility,
+      encrypted_ineligibility_reason,
+      ineligibility_expiry_time
+    )
+    VALUES (
+      -- These will resolve to NULL when call
+      -- is in the wrong state and cause the
+      -- transaction to rollback.
+      (SELECT call_id FROM mCallDetails),
+      (SELECT dog_id FROM mCallDetails),
+      (SELECT vet_id FROM mCallDetails),
+
+      $2, $3, $4, $5, $6, $7, $8, $9, $10
+    )
+    RETURNING
+      report_id,
+      dog_id,
+      vet_id,
+      report_creation_time,
+      report_modification_time
+  ),
+  mUpdateCall as (
+    UPDATE calls
+    SET
+      call_outcome = 'REPORTED'
+    WHERE call_id = $1
+    AND call_outcome = 'APPOINTMENT'
+    RETURNING call_id, call_outcome
   )
-  VALUES (
-    $1,
-    (SELECT dog_id FROM calls WHERE call_id = $1),
-    (SELECT vet_id FROM calls WHERE call_id = $1),
-    $2, $3, $4, $5, $6, $7, $8, $9, $10
-  )
-  RETURNING
+  SELECT
     report_id as "reportId",
     dog_id as "dogId",
     vet_id as "vetId",
     report_creation_time as "reportCreationTime",
-    report_modification_time as "reportModificationTime"
+    report_modification_time as "reportModificationTime",
+    call_id as "callId",
+    call_outcome as "callOutcome"
+  FROM mInsertReport, mUpdateCall
   `;
-  const res = await dbQuery(dbCtx, sql, [
+  type Row = DbReportGen & {
+    callId: string;
+    callOutcome: CallOutcome;
+  };
+  const res = await dbQuery<Row>(dbCtx, sql, [
     reportSpec.callId,
     reportSpec.visitTime,
     reportSpec.dogWeightKg,
@@ -45,5 +80,7 @@ export async function dbInsertReport(
     reportSpec.encryptedIneligibilityReason,
     reportSpec.ineligibilityExpiryTime,
   ]);
-  return res.rows[0] as DbReportGen;
+  const { callId, callOutcome, ...otherFields } = res.rows[0];
+  const reportGen: DbReportGen = otherFields;
+  return reportGen;
 }
--- tests/data/db-reports.test.ts
+++ tests/data/db-reports.test.ts
@@ -7,10 +7,12 @@ import {
   insertUser,
   insertVet,
 } from "../_fixtures";
-import { CALL_OUTCOME } from "@/lib/data/db-enums";
-import { dbInsertReport } from "@/lib/data/db-reports";
+import { CALL_OUTCOME, CallOutcome } from "@/lib/data/db-enums";
+import { dbInsertReportAndUpdateCall } from "@/lib/data/db-reports";
+import { dbQuery } from "@/lib/data/db-utils";
+import { expectError } from "../_helpers";
 
-describe("dbInsertReport", () => {
+describe("dbInsertReportAndUpdateCall", () => {
   it("should resolve dog and vet ID from the input call ID", async () => {
     await withDb(async (dbPool) => {
       const { userId } = await insertUser(1, dbPool);
@@ -24,9 +26,60 @@ describe("dbInsertReport", () => {
         CALL_OUTCOME.APPOINTMENT,
       );
       const spec = getDbReportSpec(callId);
-      const gen = await dbInsertReport(dbPool, spec);
+      const gen = await dbInsertReportAndUpdateCall(dbPool, spec);
       expect(gen.dogId).toEqual(dogId);
       expect(gen.vetId).toEqual(vetId);
+    });
+  });
+  it("should update the call outcome to REPORTED", async () => {
+    await withDb(async (dbPool) => {
+      const { userId } = await insertUser(1, dbPool);
+      const { dogId } = await insertDog(2, userId, dbPool);
+      const { vetId } = await insertVet(3, dbPool);
+      await dbInsertDogVetPreference(dbPool, dogId, vetId);
+      const { callId } = await insertCall(
+        dbPool,
+        dogId,
+        vetId,
+        CALL_OUTCOME.APPOINTMENT,
+      );
+      const spec = getDbReportSpec(callId);
+      const gen = await dbInsertReportAndUpdateCall(dbPool, spec);
+      const sql = `select call_outcome as "callOutcome" from calls where call_id = $1`;
+      const res = await dbQuery<{ callOutcome: CallOutcome }>(dbPool, sql, [
+        callId,
+      ]);
+      expect(res.rows[0].callOutcome).toEqual(CALL_OUTCOME.REPORTED);
+    });
+  });
+  it("should fail when call outcome is not initially APPOINTMENT", async () => {
+    await withDb(async (dbPool) => {
+      // GIVEN call that is DECLINED
+      const { userId } = await insertUser(1, dbPool);
+      const { dogId } = await insertDog(2, userId, dbPool);
+      const { vetId } = await insertVet(3, dbPool);
+      await dbInsertDogVetPreference(dbPool, dogId, vetId);
+      const { callId } = await insertCall(
+        dbPool,
+        dogId,
+        vetId,
+        CALL_OUTCOME.DECLINED,
+      );
+
+      // WHEN attempting to add a report
+      const spec = getDbReportSpec(callId);
+
+      // THEN error is expected
+      await expectError(async () => {
+        await dbInsertReportAndUpdateCall(dbPool, spec);
+      });
+
+      // AND call outcome should still be DECLINED.
+      const sql = `select call_outcome as "callOutcome" from calls where call_id = $1`;
+      const res = await dbQuery<{ callOutcome: CallOutcome }>(dbPool, sql, [
+        callId,
+      ]);
+      expect(res.rows[0].callOutcome).toEqual(CALL_OUTCOME.DECLINED);
     });
   });
 });
