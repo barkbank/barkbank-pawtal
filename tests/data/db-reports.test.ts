420aa82832e637e51f0358f6f77bfe13dcf1d722
Jin <hsienjin@gmail.com>
2024-04-15 09:21:47 +0800

    call-outcome - refactor tests

--- tests/data/db-reports.test.ts
+++ tests/data/db-reports.test.ts
@@ -11,75 +11,72 @@ import { CALL_OUTCOME, CallOutcome } from "@/lib/data/db-enums";
 import { dbInsertReportAndUpdateCall } from "@/lib/data/db-reports";
 import { dbQuery } from "@/lib/data/db-utils";
 import { expectError } from "../_helpers";
+import { Pool } from "pg";
+import { DbReportSpec } from "@/lib/data/db-models";
 
 describe("dbInsertReportAndUpdateCall", () => {
-  it("should resolve dog and vet ID from the input call ID", async () => {
-    await withDb(async (dbPool) => {
-      const { userId } = await insertUser(1, dbPool);
-      const { dogId } = await insertDog(2, userId, dbPool);
-      const { vetId } = await insertVet(3, dbPool);
-      await dbInsertDogVetPreference(dbPool, dogId, vetId);
-      const { callId } = await insertCall(
-        dbPool,
-        dogId,
-        vetId,
-        CALL_OUTCOME.APPOINTMENT,
-      );
-      const spec = getDbReportSpec(callId);
-      const gen = await dbInsertReportAndUpdateCall(dbPool, spec);
-      expect(gen.dogId).toEqual(dogId);
-      expect(gen.vetId).toEqual(vetId);
-    });
-  });
   it("should update the call outcome to REPORTED", async () => {
     await withDb(async (dbPool) => {
-      const { userId } = await insertUser(1, dbPool);
-      const { dogId } = await insertDog(2, userId, dbPool);
-      const { vetId } = await insertVet(3, dbPool);
-      await dbInsertDogVetPreference(dbPool, dogId, vetId);
-      const { callId } = await insertCall(
-        dbPool,
-        dogId,
-        vetId,
-        CALL_OUTCOME.APPOINTMENT,
-      );
-      const spec = getDbReportSpec(callId);
-      const gen = await dbInsertReportAndUpdateCall(dbPool, spec);
-      const sql = `select call_outcome as "callOutcome" from calls where call_id = $1`;
-      const res = await dbQuery<{ callOutcome: CallOutcome }>(dbPool, sql, [
-        callId,
-      ]);
-      expect(res.rows[0].callOutcome).toEqual(CALL_OUTCOME.REPORTED);
+      // GIVEN an APPOINTMENT
+      const { callId, reportSpec } = await initScenario(dbPool, {
+        callOutcome: CALL_OUTCOME.APPOINTMENT,
+      });
+
+      // WHEN a report is submitted
+      await dbInsertReportAndUpdateCall(dbPool, reportSpec);
+
+      // THEN call outcome should be REPORTED
+      const { callOutcome } = await fetchCallOutcome(dbPool, callId);
+      expect(callOutcome).toEqual(CALL_OUTCOME.REPORTED);
     });
   });
   it("should fail when call outcome is not initially APPOINTMENT", async () => {
     await withDb(async (dbPool) => {
       // GIVEN call that is DECLINED
-      const { userId } = await insertUser(1, dbPool);
-      const { dogId } = await insertDog(2, userId, dbPool);
-      const { vetId } = await insertVet(3, dbPool);
-      await dbInsertDogVetPreference(dbPool, dogId, vetId);
-      const { callId } = await insertCall(
-        dbPool,
-        dogId,
-        vetId,
-        CALL_OUTCOME.DECLINED,
-      );
+      const { callId, reportSpec } = await initScenario(dbPool, {
+        callOutcome: CALL_OUTCOME.DECLINED,
+      });
 
-      // WHEN attempting to add a report
-      const spec = getDbReportSpec(callId);
-
-      // THEN error is expected
+      // THEN error is expected WHEN inserting the report
       await expectError(async () => {
-        await dbInsertReportAndUpdateCall(dbPool, spec);
+        await dbInsertReportAndUpdateCall(dbPool, reportSpec);
       });
 
       // AND call outcome should still be DECLINED.
-      const sql = `select call_outcome as "callOutcome" from calls where call_id = $1`;
-      const res = await dbQuery<{ callOutcome: CallOutcome }>(dbPool, sql, [
-        callId,
-      ]);
-      expect(res.rows[0].callOutcome).toEqual(CALL_OUTCOME.DECLINED);
+      const { callOutcome } = await fetchCallOutcome(dbPool, callId);
+      expect(callOutcome).toEqual(CALL_OUTCOME.DECLINED);
     });
   });
 });
+
+async function initScenario(
+  dbPool: Pool,
+  args: { callOutcome: CallOutcome },
+): Promise<{ callId: string; reportSpec: DbReportSpec }> {
+  const { callOutcome } = args;
+
+  const { userId } = await insertUser(1, dbPool);
+  const { dogId } = await insertDog(2, userId, dbPool);
+  const { vetId } = await insertVet(3, dbPool);
+  await dbInsertDogVetPreference(dbPool, dogId, vetId);
+  const { callId } = await insertCall(dbPool, dogId, vetId, callOutcome);
+
+  // WHEN attempting to add a report
+  const reportSpec: DbReportSpec = getDbReportSpec(callId);
+  return { callId, reportSpec };
+}
+
+async function fetchCallOutcome(
+  dbPool: Pool,
+  callId: string,
+): Promise<{ callOutcome: CallOutcome }> {
+  const sql = `
+  select call_outcome as "callOutcome"
+  from calls
+  where call_id = $1
+  `;
+  const res = await dbQuery<{ callOutcome: CallOutcome }>(dbPool, sql, [
+    callId,
+  ]);
+  return res.rows[0];
+}
