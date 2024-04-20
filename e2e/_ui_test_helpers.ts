b0159521a350d7fd3165cc846a085ee71d4c7231
Jin <hsienjin@gmail.com>
2024-04-20 23:49:55 +0800

    register-test-user - impl registerTestUser

--- e2e/_ui_test_helpers.ts
+++ e2e/_ui_test_helpers.ts
@@ -1,5 +1,8 @@
 import { RoutePath } from "@/lib/route-path";
+import { generateRandomGUID } from "@/lib/utilities/bark-guid";
+import { formatDateTime, SINGAPORE_TIME_ZONE } from "@/lib/utilities/bark-time";
 import { expect, Page } from "@playwright/test";
+import { sprintf } from "sprintf-js";
 
 export function urlOf(path: string): string {
   return `http://localhost:3000${path}`;
@@ -28,6 +31,83 @@ export const UI_VET = {
 export const UI_ADMIN = {
   ADMIN_EMAIL: "admin1@admin.com",
 };
+
+export function generateTestDogName(): string {
+  const tsid = formatDateTime(new Date(), {
+    format: "d MMM yyyy",
+    timeZone: SINGAPORE_TIME_ZONE,
+  });
+  const guid = generateRandomGUID(6);
+  return `Kelper (${tsid}, ${guid})`;
+}
+
+export function getTestBirthday(ageYears: number): string {
+  const ts = new Date();
+  const y = ts.getUTCFullYear() - ageYears;
+  const m = ts.getUTCMonth() + 1;
+  const d = ts.getUTCDate();
+  const ymd = sprintf("%04d-%02d-%02d", y, m, d);
+  return ymd;
+}
+
+export async function registerTestUser(args: { page: Page }): Promise<{
+  guid: string;
+  userName: string;
+  userEmail: string;
+  userPhoneNumber: string;
+  dogName: string;
+  dogBreed: string;
+}> {
+  const guid = generateRandomGUID(8);
+  const userName = `Alice (${guid})`;
+  const userEmail = `alice.${guid}@user.com`;
+  const userPhoneNumber = guid;
+  const dogName = `Bob (${guid})`;
+  const dogBreed = "REGISTERED DOG";
+
+  const { page } = args;
+  await page.goto(urlOf(RoutePath.USER_REGISTRATION));
+  await expect(page).toHaveURL(urlOf(RoutePath.USER_REGISTRATION));
+
+  // Pet Form
+  await page.getByLabel("What's your dog's name?").fill(dogName);
+  await page.getByLabel("What's your dog's breed?").fill(dogBreed);
+  await page.locator('input[name="dogBirthday"]').fill(getTestBirthday(5));
+  await page.getByRole("button", { name: "Male", exact: true }).click();
+  await page.getByLabel("What's your dog's weight? (KG)").fill("31.4");
+  await page
+    .getByText("Do you know it's blood type?I")
+    .getByLabel("I don't know")
+    .click();
+  await page
+    .getByText("Has it received blood transfusion before?I don't knowYesNo")
+    .locator('[id="\\:R11rrrqkq\\:-form-item"]')
+    .getByRole("button", { name: "No", exact: true })
+    .click();
+  await page
+    .getByText("Has your dog been pregnant before?I don't knowYesNo")
+    .locator('[id="\\:R15rrrqkq\\:-form-item"]')
+    .getByRole("button", { name: "No", exact: true })
+    .click();
+  await page.getByLabel("Vet Clinic 1").click();
+  await page.getByRole("button", { name: "Next" }).click();
+
+  // Human Form
+  await page
+    .getByText("Are you currently based in Singapore?YesNo")
+    .getByRole("button", { name: "Yes" })
+    .click();
+  await page.getByLabel("How would you like to be").fill(userName);
+  await page.getByLabel("What number can we reach you").fill(userPhoneNumber);
+  await page.getByLabel("Please provide a login email").fill(userEmail);
+  await page.getByLabel("Enter OTP").fill("000000");
+  await page.getByLabel("Disclaimer").click();
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  await page.getByRole("button", { name: "Enter My Dashboard" }).click();
+  await expect(page).toHaveURL(urlOf(RoutePath.USER_DEFAULT_LOGGED_IN_PAGE));
+  return { guid, userName, userEmail, userPhoneNumber, dogName, dogBreed };
+}
 
 export async function loginTestUser(args: { page: Page }) {
   const { page } = args;
