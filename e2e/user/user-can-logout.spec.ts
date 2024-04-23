db185518120da6fc45a96a3dde8c6ec1f91beabe
Jin <hsienjin@gmail.com>
2024-04-23 09:21:55 +0800

    pom-clean-up - clean up the logout sequence - accepts context only, not a pom page.

--- e2e/user/user-can-logout.spec.ts
+++ e2e/user/user-can-logout.spec.ts
@@ -3,6 +3,7 @@ import { initLoginKnownUser } from "../_lib/pom/init";
 import { doLogoutSequence } from "../_lib/sequences/logout-sequence";
 
 test("user can logout", async ({ page }) => {
-  const { knownUser, pomPage } = await initLoginKnownUser(page);
-  await doLogoutSequence(pomPage);
+  const { pomPage } = await initLoginKnownUser(page);
+  const context = pomPage.context();
+  await doLogoutSequence({ context });
 });
