76415d0b7a43453a12a8e9f100ff2ea5c5349abf
Brennan <tanjialebrennan@gmail.com>
2024-04-26 00:45:01 +0800

    test: update e2e tests

--- e2e/user/user-can-cancel-edit-account.spec.ts
+++ e2e/user/user-can-cancel-edit-account.spec.ts
@@ -1,19 +1,21 @@
 import { test, expect } from "@playwright/test";
-import { loginKnownUser } from "../_lib/pom/init/login-known-user";
 import { gotoUserMyAccountPage } from "../_lib/sequences/nav-gotos";
 import { UserMyAccountEditPage } from "../_lib/pom/pages/user-my-account-edit-page";
+import { registerTestUser } from "../_lib/pom/init/register-test-user";
 
-test("user can login, go to edit page, but cancel, and should not see their details changed", async ({
+test("user can register, edit account details, but click cancel, and should see their details unchanged", async ({
   page,
 }) => {
   // GIVEN
-  const { context, knownUser } = await loginKnownUser({ page });
+  const { context, userName, userEmail, userPhoneNumber } =
+    await registerTestUser({
+      page,
+    });
   const accountPage = await gotoUserMyAccountPage({ context });
-  const { userName, userEmail, userPhoneNumber, userResidency } = knownUser;
   await expect(accountPage.exactText(userName)).toBeVisible();
+  await expect(accountPage.exactText("Singapore")).toBeVisible();
   await expect(accountPage.exactText(userEmail)).toBeVisible();
   await expect(accountPage.exactText(userPhoneNumber)).toBeVisible();
-  await expect(accountPage.exactText(userResidency)).toBeVisible();
 
   // WHEN user clicks on edit button, navigate to edit page
   await accountPage.editButton().click();
@@ -21,11 +23,15 @@ test("user can login, go to edit page, but cancel, and should not see their deta
   await editAccountPage.checkUrl();
 
   // BUT cancelled, navigate back to account page
+  await editAccountPage.userNameField().fill("New Name");
+  await editAccountPage.userPhoneNumberField().fill("+65 12345678");
+  await editAccountPage.userResidencyOption_OTHERS().click();
   await editAccountPage.cancelButton().click();
 
   // THEN
+  await accountPage.checkUrl();
   await expect(accountPage.exactText(userName)).toBeVisible();
+  await expect(accountPage.exactText("Singapore")).toBeVisible();
   await expect(accountPage.exactText(userEmail)).toBeVisible();
   await expect(accountPage.exactText(userPhoneNumber)).toBeVisible();
-  await expect(accountPage.exactText(userResidency)).toBeVisible();
 });
--- e2e/user/user-can-edit-account.spec.ts
+++ e2e/user/user-can-edit-account.spec.ts
@@ -1,19 +1,21 @@
 import { test, expect } from "@playwright/test";
-import { loginKnownUser } from "../_lib/pom/init/login-known-user";
 import { gotoUserMyAccountPage } from "../_lib/sequences/nav-gotos";
 import { UserMyAccountEditPage } from "../_lib/pom/pages/user-my-account-edit-page";
+import { registerTestUser } from "../_lib/pom/init/register-test-user";
 
-test("user can login, go to edit page, but cancel, and should not see their details changed", async ({
+test("user can register, edit account details, save, and should see their details changed", async ({
   page,
 }) => {
   // GIVEN
-  const { context, knownUser } = await loginKnownUser({ page });
+  const { context, userName, userEmail, userPhoneNumber } =
+    await registerTestUser({
+      page,
+    });
   const accountPage = await gotoUserMyAccountPage({ context });
-  const { userName, userEmail, userPhoneNumber, userResidency } = knownUser;
   await expect(accountPage.exactText(userName)).toBeVisible();
+  await expect(accountPage.exactText("Singapore")).toBeVisible();
   await expect(accountPage.exactText(userEmail)).toBeVisible();
   await expect(accountPage.exactText(userPhoneNumber)).toBeVisible();
-  await expect(accountPage.exactText(userResidency)).toBeVisible();
 
   // WHEN user navigates to edit page, and saves
   await accountPage.editButton().click();
@@ -25,8 +27,9 @@ test("user can login, go to edit page, but cancel, and should not see their deta
   await editAccountPage.saveButton().click();
 
   // THEN
+  await accountPage.checkUrl();
   await expect(accountPage.exactText("New Name")).toBeVisible();
+  await expect(accountPage.exactText("Other")).toBeVisible();
   await expect(accountPage.exactText(userEmail)).toBeVisible();
   await expect(accountPage.exactText("+65 12345678")).toBeVisible();
-  await expect(accountPage.exactText("Other")).toBeVisible();
 });
