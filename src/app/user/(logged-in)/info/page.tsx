056ed4819787368e6bda5026bef6f9623860b244
Brennan <tanjialebrennan@gmail.com>
2024-04-27 11:27:32 +0800

    feat: update top section styling

--- src/app/user/(logged-in)/info/page.tsx
+++ src/app/user/(logged-in)/info/page.tsx
@@ -41,30 +41,30 @@ export default async function Page() {
     <main className="flex flex-col 2xl:items-center">
       <section className="mx-[60px] mt-[60px] max-w-7xl py-[45px] text-center">
         <BarkH2>Criteria For Blood Donation</BarkH2>
-        <BarkP classes="text-sm">
+        <BarkP>
           To be eligible for blood donation, dogs must meet specific criteria to
           ensure the safety of both donors and recipients. It is advisable to
           consult with your veterinarian for a comprehensive understanding of
           these criteria. The following are the typical criteria:
         </BarkP>
       </section>
-      <section className="mb-[60px] grid max-w-7xl grid-cols-2 gap-x-[60px] px-4 text-sm md:mx-0 md:grid-cols-3 md:gap-x-[100px] md:gap-y-[60px] md:px-[60px]">
-        {criteras.map((criteria) => (
+      <section className="mb-[60px] grid grid-cols-1 place-items-center gap-[60px] px-4 sm:grid-cols-2 lg:grid-cols-3">
+        {criteras.map((criteria, i) => (
           <div
             key={criteria.text}
-            className="flex min-w-40 flex-col items-center space-y-4 md:max-w-60 "
+            className={`flex flex-col items-center gap-y-4 ${criteras.length % 2 !== 0 && i === criteras.length - 1 ? "sm:col-span-2 lg:col-span-1" : ""}`}
           >
             <Image
               src={criteria.imgUrl}
               alt="criteria icon"
-              className="h-[50px] w-[50px] md:h-[100px] md:w-[100px]"
+              className="lg:h-[100px] lg:w-[100px]"
               width={50}
               height={50}
             />
-            <p className="text-center">{criteria.text}</p>
+            <p className="max-w-[60%] text-center">{criteria.text}</p>
           </div>
         ))}
-      </section>     
+      </section>
     </main>
   );
 }
