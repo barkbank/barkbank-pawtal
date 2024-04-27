8a3ceb5c1bd4c3f3be20617d5c9f59f301ed569f
Jin <hsienjin@gmail.com>
2024-04-27 15:09:21 +0800

    edit-dog - stub edit dog page

---
+++ src/app/user/(logged-in)/my-pets/dogs/[dogId]/edit/page.tsx
@@ -0,0 +1,13 @@
+import { BarkButton } from "@/components/bark/bark-button";
+import { BarkH1 } from "@/components/bark/bark-typography";
+import { RoutePath } from "@/lib/route-path";
+
+export default async function Page(props: { params: {dogId: string}}) {
+  const {params: {dogId}} = props;
+  return (
+    <div className="p-3">
+      <BarkH1>Edit Dog {dogId}</BarkH1>
+      <BarkButton variant="brandInverse" href={RoutePath.USER_MY_PETS}>Back</BarkButton>
+    </div>
+  );
+}
