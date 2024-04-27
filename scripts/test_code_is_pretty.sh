592a7926e61c462c23a088137e5c11b0279ae8ec
Jin <hsienjin@gmail.com>
2024-04-27 09:53:13 +0800

    imp-run-tests - some blank lines to separate diff, status, and instruction

--- scripts/test_code_is_pretty.sh
+++ scripts/test_code_is_pretty.sh
@@ -10,13 +10,17 @@ if [[ -z $(git status -s) ]]; then
 fi
 
 # Print debugging information otherwise.
+echo
+echo
 echo "=== git diff ==="
 git diff
-
+echo
+echo
 echo "=== git status ==="
 git status
-
+echo
+echo
 echo "=== instruction ==="
 echo "run 'make fmt' or 'npm run fmt' to address the above formatting issues"
-
+echo
 exit 1
