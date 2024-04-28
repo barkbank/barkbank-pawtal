e9db9746881aa5df611fca75dfa42452e1834e87
Jin <hsienjin@gmail.com>
2024-04-28 15:42:05 +0800

    tmp1 - delete all the tests except one

--- e2e/user/user-can-logout.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "@playwright/test";
-import { loginKnownUser } from "../_lib/pom/init/login-known-user";
-import { doLogoutSequence } from "../_lib/sequences/logout-sequence";
-
-test("user can logout", async ({ page }) => {
-  const { pomPage } = await loginKnownUser({ page });
-  const context = pomPage.context();
-  await doLogoutSequence({ context });
-});
