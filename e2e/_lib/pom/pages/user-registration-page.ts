c0667e8bd733f00975cb131c6007e251feef377f
Jin <hsienjin@gmail.com>
2024-04-28 23:58:32 +0800

    test visitor-can-register (#140)
    (1) Test the user registration flow.
    
    (2) Details
    
    —(a) A UserRegistrationPage POM was defined.
    
    —(b) A visitor-can-register scenario was written using the
    UserRegistrationPage POM.
    
    —(c) The register-test-user init function was updated to use the new
    UserRegistrationPage POM.
    
    (3) Other changes
    
    —(a) Use matrix strategy to run chromium and mobile chrome tests in
    parallel.

---
+++ e2e/_lib/pom/pages/user-registration-page.ts
@@ -0,0 +1,177 @@
+import { RoutePath } from "@/lib/route-path";
+import { PomPage } from "../core/pom-page";
+import { Locator } from "@playwright/test";
+
+export class UserRegistrationPage extends PomPage {
+  url(): string {
+    return this.website().urlOf(RoutePath.USER_REGISTRATION);
+  }
+
+  dogFormHeader(): Locator {
+    return this.page()
+      .locator("form")
+      .getByText("Tell us about your pet", { exact: true });
+  }
+
+  ownerFormHeader(): Locator {
+    return this.page()
+      .locator("form")
+      .getByText("Add your details", { exact: true });
+  }
+
+  dogNameField(): Locator {
+    return this.page().getByLabel("What's your dog's name?");
+  }
+
+  dogBreedField(): Locator {
+    return this.page().getByLabel("What's your dog's breed?");
+  }
+
+  dogBirthdayField(): Locator {
+    return this.page().locator('input[name="dogBirthday"]');
+  }
+
+  dogGender_MALE(): Locator {
+    return this.page().getByRole("button", { name: "Male", exact: true });
+  }
+
+  dogGender_FEMALE(): Locator {
+    return this.page().getByRole("button", { name: "Female", exact: true });
+  }
+
+  dogWeightField(): Locator {
+    return this.page().getByLabel("What's your dog's weight? (KG)");
+  }
+
+  private dogBloodTypeOptions(): Locator {
+    return this.page().getByText("Do you know it's blood type?").locator("..");
+  }
+
+  dogBloodType_UNKNOWN(): Locator {
+    return this.dogBloodTypeOptions().getByLabel("I don't know");
+  }
+
+  dogBloodType_POSITIVE(): Locator {
+    return this.dogBloodTypeOptions().getByLabel("D.E.A 1.1 positive");
+  }
+
+  dogBloodType_NEGATIVE(): Locator {
+    return this.dogBloodTypeOptions().getByLabel("D.E.A 1.1 negative");
+  }
+
+  private dogEverReceivedTransfusionOptions(): Locator {
+    return this.page().getByText("Has it received blood").locator("..");
+  }
+
+  dogEverReceivedTransfusion_UNKNOWN(): Locator {
+    return this.dogEverReceivedTransfusionOptions().getByRole("button", {
+      name: "I don't know",
+      exact: true,
+    });
+  }
+
+  dogEverReceivedTransfusion_YES(): Locator {
+    return this.dogEverReceivedTransfusionOptions().getByRole("button", {
+      name: "Yes",
+      exact: true,
+    });
+  }
+
+  dogEverReceivedTransfusion_NO(): Locator {
+    return this.dogEverReceivedTransfusionOptions().getByRole("button", {
+      name: "No",
+      exact: true,
+    });
+  }
+
+  private dogEverPregnantOptions(): Locator {
+    return this.page().getByText("Has your dog been pregnant").locator("..");
+  }
+
+  dogEverPregnant_UNKNOWN(): Locator {
+    return this.dogEverPregnantOptions().getByRole("button", {
+      name: "I don't know",
+      exact: true,
+    });
+  }
+
+  dogEverPregnant_YES(): Locator {
+    return this.dogEverPregnantOptions().getByRole("button", {
+      name: "Yes",
+      exact: true,
+    });
+  }
+
+  dogEverPregnant_NO(): Locator {
+    return this.dogEverPregnantOptions().getByRole("button", {
+      name: "No",
+      exact: true,
+    });
+  }
+
+  dogPreferredVet_VetClinic1(): Locator {
+    return this.page().getByLabel("Vet Clinic 1");
+  }
+
+  nextButton(): Locator {
+    return this.page().getByRole("button", { name: "Next" });
+  }
+
+  cancelButton(): Locator {
+    return this.page().getByRole("button", { name: "Cancel" });
+  }
+
+  userResidency_SINGAPORE(): Locator {
+    return this.page()
+      .getByText("Are you currently based in Singapore")
+      .locator("..")
+      .getByRole("button", { name: "Yes", exact: true });
+  }
+
+  userResidency_OTHER(): Locator {
+    return this.page()
+      .getByText("Are you currently based in Singapore")
+      .locator("..")
+      .getByRole("button", { name: "No", exact: true });
+  }
+
+  userNameField(): Locator {
+    return this.page().getByLabel("How would you like to be");
+  }
+
+  userPhoneNumberField(): Locator {
+    return this.page().getByLabel("What number can we reach you");
+  }
+
+  userEmailField(): Locator {
+    return this.page().getByLabel("Please provide a login email");
+  }
+
+  sendOtpButton(): Locator {
+    return this.page().getByRole("button", { name: "Send me an OTP" });
+  }
+
+  disclaimerCheckbox(): Locator {
+    return this.page().getByLabel("Disclaimer");
+  }
+
+  otpField(): Locator {
+    return this.page().getByLabel("Enter OTP");
+  }
+
+  submitButton(): Locator {
+    return this.page().getByRole("button", { name: "Submit" });
+  }
+
+  backButton(): Locator {
+    return this.page().getByRole("button", { name: "Back" });
+  }
+
+  accountCreatedMessage(): Locator {
+    return this.page().getByText("your account has been created");
+  }
+
+  enterDashboardButton(): Locator {
+    return this.page().getByRole("button", { name: "Enter My Dashboard" });
+  }
+}
---
+++ e2e/visitor/visitor-can-register.spec.ts
@@ -0,0 +1,77 @@
+import { test, expect } from "@playwright/test";
+import { initPomContext } from "../_lib/pom/init/init-pom-context";
+import { UserLoginPage } from "../_lib/pom/pages/user-login-page";
+import { UserRegistrationPage } from "../_lib/pom/pages/user-registration-page";
+import { getTestBirthday } from "../_lib/e2e-test-utils";
+import { generateRandomGUID } from "@/lib/utilities/bark-guid";
+import { NavComponent } from "../_lib/pom/layout/nav-component";
+import { UserMyAccountPage } from "../_lib/pom/pages/user-my-account-page";
+import { UserMyPetsPage } from "../_lib/pom/pages/user-my-pets-page";
+
+test("visitor can register a new user account", async ({ page }) => {
+  // Navigating to the registration form
+  const context = await initPomContext({ page });
+  const pg1 = new UserLoginPage(context);
+  await pg1.checkUrl();
+  await pg1.registerLink().click();
+
+  // Fill in the dog form
+  const pg2 = new UserRegistrationPage(context);
+  await pg2.checkUrl();
+  await expect(pg2.dogFormHeader()).toBeVisible();
+  await pg2.dogNameField().fill("Casper");
+  await pg2.dogBreedField().fill("White Sheet Dog");
+  const dogBirthday = getTestBirthday(6);
+  await pg2.dogBirthdayField().fill(dogBirthday);
+  await pg2.dogWeightField().fill("42");
+  await pg2.dogGender_MALE().click();
+  await pg2.dogBloodType_UNKNOWN().click();
+  await pg2.dogEverPregnant_NO().click();
+  await pg2.dogEverReceivedTransfusion_NO().click();
+  await pg2.dogPreferredVet_VetClinic1().click();
+  await pg2.nextButton().click();
+
+  // Fill in the owner form
+  await expect(pg2.ownerFormHeader()).toBeVisible();
+  await pg2.userResidency_SINGAPORE().click();
+  await pg2.userNameField().fill("Ian Little");
+  await pg2.userPhoneNumberField().fill("87654321");
+  const guid = generateRandomGUID(8);
+  const userEmail = `ian.${guid}@user.com`;
+  await pg2.userEmailField().fill(userEmail);
+
+  // Go back to the dog form and back to the owner form. This checks if the data
+  // entered for dog and owner are preserved.
+  await pg2.backButton().click();
+  await expect(pg2.dogFormHeader()).toBeVisible();
+  await pg2.nextButton().click();
+  await expect(pg2.ownerFormHeader()).toBeVisible();
+
+  // Request OTP and submit
+  await pg2.sendOtpButton().click();
+  await pg2.otpField().fill("000000");
+  await pg2.disclaimerCheckbox().click();
+  await pg2.submitButton().click();
+
+  // Registered page. Click enter dashboard button.
+  await expect(pg2.accountCreatedMessage()).toBeVisible();
+  await expect(pg2.enterDashboardButton()).toBeVisible();
+  await pg2.enterDashboardButton().click();
+
+  // Check for expected dog in my pets
+  const pg3 = new UserMyPetsPage(context);
+  await pg3.checkUrl();
+  await expect(pg3.dogCardItem("Casper").locator()).toBeVisible();
+
+  // Navigate to my account page
+  const nav = new NavComponent(context);
+  await expect(nav.locator()).toBeVisible();
+  await nav.myAcountOption().click();
+
+  // Check for account details
+  const pg4 = new UserMyAccountPage(context);
+  await pg4.checkUrl();
+  await expect(pg4.exactText("Ian Little")).toBeVisible();
+  await expect(pg4.exactText("87654321")).toBeVisible();
+  await expect(pg4.exactText(userEmail)).toBeVisible();
+});
