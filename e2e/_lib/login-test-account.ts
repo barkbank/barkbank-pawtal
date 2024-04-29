17df0c95fed181e0436995dfc013320412303e13
Jin <hsienjin@gmail.com>
2024-04-29 21:13:34 +0800

    ui-test-cleanup - remove login-test-account.ts

--- e2e/_lib/login-test-account.ts
+++
@@ -1,9 +0,0 @@
-// WIP: remove this when no longer used
-export const UI_USER = {
-  USER_EMAIL: "test_user@user.com",
-  USER_NAME: "Tess Yu Ser",
-  USER_PHONE_NUMBER: "+65 30002000",
-  ELIGIBLE_DOG_NAME: "Mape",
-  TEMPORARILY_INELIGIBLE_DOG_NAME: "Ridley",
-  PERMANENTLY_INELIGIBLE_DOG_NAME: "Perry",
-} as const;
