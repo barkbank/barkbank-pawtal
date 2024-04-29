b43488f6008465c9fbf99ee586211cb3d6f907d5
Jin <hsienjin@gmail.com>
2024-04-29 17:45:24 +0800

    ui-test-cleanup - test root page redirections

--- e2e/_lib/init/login-known-admin.ts
+++ e2e/_lib/init/login-known-admin.ts
@@ -2,10 +2,10 @@ import { Page, expect } from "@playwright/test";
 import { PomContext } from "../pom/core/pom-object";
 import { PomAdmin } from "../pom/entities";
 import { initPomContext } from "./init-pom-context";
-import { VetLoginPage } from "../pom/pages/vet-login-page";
 import { FooterComponent } from "../pom/layout/footer-component";
 import { RoutePath } from "@/lib/route-path";
 import { getKnownAdmin } from "../known-entities/known-admin";
+import { AdminLoginPage } from "../pom/pages/admin-login-page";
 
 export async function loginKnownAdmin(args: { page: Page }): Promise<{
   context: PomContext;
@@ -15,9 +15,9 @@ export async function loginKnownAdmin(args: { page: Page }): Promise<{
   const context = await initPomContext({ page });
   const knownAdmin = getKnownAdmin();
   const footer = new FooterComponent(context);
-  await footer.vetLoginLink().click();
+  await footer.adminLoginLink().click();
 
-  const pg = new VetLoginPage(context);
+  const pg = new AdminLoginPage(context);
   await pg.checkUrl();
   await pg.emailField().fill(knownAdmin.adminEmail);
   await pg.otpField().fill("000000");
