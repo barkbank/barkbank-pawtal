4c65f673d9e93205bf379629b51114eeab6e51cc
Jin <hsienjin@gmail.com>
2024-05-01 12:10:29 +0800

    view-dog-page - fetch profile, statuses, and appointments for view dog page

--- src/app/user/(logged-in)/my-pets/view-dog/[dogId]/page.tsx
+++ src/app/user/(logged-in)/my-pets/view-dog/[dogId]/page.tsx
@@ -1,20 +1,39 @@
 import { getAuthenticatedUserActor } from "@/lib/auth";
 import { RoutePath } from "@/lib/route-path";
+import { getDogAppointments } from "@/lib/user/actions/get-dog-appointments";
+import { getDogProfile } from "@/lib/user/actions/get-dog-profile";
+import { getDogStatuses } from "@/lib/user/actions/get-dog-statuses";
 import { redirect } from "next/navigation";
 
 export default async function Page(props: { params: { dogId: string } }) {
-  const {
-    params: { dogId },
-  } = props;
-
+  const { dogId } = props.params;
   const actor = await getAuthenticatedUserActor();
   if (actor === null) {
     redirect(RoutePath.USER_LOGIN_PAGE);
   }
+  const [resDogProfile, resDogStatuses, resDogAppointments] = await Promise.all(
+    [
+      getDogProfile(actor, dogId),
+      getDogStatuses(actor, dogId),
+      getDogAppointments(actor, dogId),
+    ],
+  );
 
   return (
     <div className="m-3">
       <p>View Dog {dogId}</p>
+
+      <p>
+        <pre className="text-xs">{JSON.stringify(resDogProfile, null, 2)}</pre>
+      </p>
+      <p>
+        <pre className="text-xs">{JSON.stringify(resDogStatuses, null, 2)}</pre>
+      </p>
+      <p>
+        <pre className="text-xs">
+          {JSON.stringify(resDogAppointments, null, 2)}
+        </pre>
+      </p>
     </div>
   );
 }
