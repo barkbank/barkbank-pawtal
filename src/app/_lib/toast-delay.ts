651bff1942a32aac3f1edf5912f32dea60ff6f26
Jin <hsienjin@gmail.com>
2024-05-03 22:11:40 +0800

    delayed-toast - remove TOAST_DELAY_MILLIS - no longer used

--- src/app/_lib/toast-delay.ts
+++ src/app/_lib/toast-delay.ts
@@ -1,11 +1,4 @@
 /**
- * A delay amount before showing toasts like Saving..., Adding..., etc. Because
- * if an action is fast enough (under this delay) we want to go straight to
- * Saved or Added.
- */
-export const TOAST_DELAY_MILLIS = 250;
-
-/**
  * The minimum number of milliseconds to display a toast.
  */
 export const MINIMUM_TOAST_MILLIS = 1337;
