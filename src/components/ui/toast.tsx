98c56805a6eaad0a1dc1ae84b2cbdf808450b8f3
Jin <hsienjin@gmail.com>
2024-05-03 08:43:06 +0800

    saving-dog - add brand variants to toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -32,10 +32,13 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
+        brandInfo: "border bg-brand-light text-foreground",
+        brandSuccess: "border bg-brand-light text-foreground",
+        brandError: "border bg-brand text-brand-light",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "brandInfo",
     },
   },
 );
