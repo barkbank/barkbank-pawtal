a0b12d3b85d08aebf89078a317f84c2e2e1b6817
Jin <hsienjin@gmail.com>
2024-05-26 22:19:28 +0800

    report-management - update add appointment test

--- tests/bark/add-appointment.test.ts
+++ tests/bark/add-appointment.test.ts
@@ -1,40 +1,53 @@
 import { CODE } from "@/lib/utilities/bark-code";
-import { withService } from "./_service";
+import { withBarkContext } from "./_service";
 import { givenDog, givenVet } from "./_given";
+import { BarkAction_addAppointment } from "@/lib/bark/actions/add-appointment";
 
-describe("addAppointment", () => {
+describe("BarkAction_addAppointment", () => {
   it("should return ERROR_DOG_NOT_FOUND when specified dog cannot be found", async () => {
-    await withService(async ({ service }) => {
+    await withBarkContext(async ({ context }) => {
       const dogId = "12345";
       const vetId = "67890";
-      const { result, error } = await service.addAppointment({ dogId, vetId });
+      const { result, error } = await BarkAction_addAppointment(context, {
+        dogId,
+        vetId,
+      });
       expect(result).toBeUndefined();
       expect(error).toEqual(CODE.ERROR_DOG_NOT_FOUND);
     });
   });
   it("should return ERROR_VET_NOT_FOUND when specified vet cannot be found", async () => {
-    await withService(async ({ service, context }) => {
-      const { dogId } = await givenDog(context);
+    await withBarkContext(async ({ context, testContext }) => {
+      const { dogId } = await givenDog(testContext);
       const vetId = "67890";
-      const { result, error } = await service.addAppointment({ dogId, vetId });
+      const { result, error } = await BarkAction_addAppointment(context, {
+        dogId,
+        vetId,
+      });
       expect(result).toBeUndefined();
       expect(error).toEqual(CODE.ERROR_VET_NOT_FOUND);
     });
   });
   it("should return ERROR_NOT_PREFERRED_VET when specified vet not preferred vet of the specified dog", async () => {
-    await withService(async ({ service, context }) => {
-      const { dogId } = await givenDog(context);
-      const { vetId } = await givenVet(context);
-      const { result, error } = await service.addAppointment({ dogId, vetId });
+    await withBarkContext(async ({ context, testContext }) => {
+      const { dogId } = await givenDog(testContext);
+      const { vetId } = await givenVet(testContext);
+      const { result, error } = await BarkAction_addAppointment(context, {
+        dogId,
+        vetId,
+      });
       expect(result).toBeUndefined();
       expect(error).toEqual(CODE.ERROR_NOT_PREFERRED_VET);
     });
   });
   it("should return appointment id", async () => {
-    await withService(async ({ service, context }) => {
-      const { vetId } = await givenVet(context);
-      const { dogId } = await givenDog(context, { preferredVetId: vetId });
-      const { result, error } = await service.addAppointment({ dogId, vetId });
+    await withBarkContext(async ({ context, testContext }) => {
+      const { vetId } = await givenVet(testContext);
+      const { dogId } = await givenDog(testContext, { preferredVetId: vetId });
+      const { result, error } = await BarkAction_addAppointment(context, {
+        dogId,
+        vetId,
+      });
       expect(result).not.toBeUndefined();
       expect(error).toBeUndefined();
       const { appointmentId } = result!;
@@ -42,14 +55,17 @@ describe("addAppointment", () => {
     });
   });
   it("should return ERROR_APPOINTMENT_ALREADY_EXISTS when appointment already exists", async () => {
-    await withService(async ({ service, context }) => {
-      const { vetId } = await givenVet(context);
-      const { dogId } = await givenDog(context, { preferredVetId: vetId });
+    await withBarkContext(async ({ context, testContext }) => {
+      const { vetId } = await givenVet(testContext);
+      const { dogId } = await givenDog(testContext, { preferredVetId: vetId });
       // Make first appointment
-      await service.addAppointment({ dogId, vetId });
+      await BarkAction_addAppointment(context, { dogId, vetId });
 
       // Second appointment should get error.
-      const { result, error } = await service.addAppointment({ dogId, vetId });
+      const { result, error } = await BarkAction_addAppointment(context, {
+        dogId,
+        vetId,
+      });
       expect(result).toBeUndefined();
       expect(error).toEqual(CODE.ERROR_APPOINTMENT_ALREADY_EXISTS);
     });
