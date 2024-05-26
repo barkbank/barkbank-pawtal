c9df7c74330a3627257b57b8301aae95aa32d06d
Jin <hsienjin@gmail.com>
2024-05-26 22:29:06 +0800

    report-management - remove unused sql files

--- src/lib/pg-bark/_sql/insert-appointment.sql
+++
@@ -1,6 +0,0 @@
--- WIP: remove this sql
--- (dogId, vetId)
-
-INSERT INTO calls (dog_id, vet_id, call_outcome, encrypted_opt_out_reason)
-VALUES ($1, $2, 'APPOINTMENT', '')
-RETURNING call_id::text as "appointmentId"
--- src/lib/pg-bark/_sql/load-sql.ts
+++ src/lib/pg-bark/_sql/load-sql.ts
@@ -2,9 +2,6 @@ import { ObjectValues } from "@/lib/utilities/object-values";
 import { dbLoadSql } from "../../data/db-load-sql";
 
 export const SQL_QUERY = {
-  INSERT_APPOINTMENT: "insert-appointment",
-  INSERT_REPORT_UPDATE_CALL: "insert-report-update-call",
-  SELECT_APPOINTMENT_EXISTS: "select-appointment-exists",
   SELECT_CAN_SCHEDULE: "select-can-schedule",
   SELECT_REPORT: "select-report",
   UPDATE_REPORT: "update-report",
