69fbf48062b6f42dfa7d7f040d8b1d8159790098
Jin <hsienjin@gmail.com>
2024-05-26 21:32:46 +0800

    report-management - simplify createReport

--- src/lib/pg-bark/_impls/create-report.ts
+++ src/lib/pg-bark/_impls/create-report.ts
@@ -2,16 +2,11 @@ import { Err, Ok, Result } from "@/lib/utilities/result";
 import { CODE } from "@/lib/utilities/bark-code";
 import { PgBarkServiceConfig } from "../pg-bark-service";
 import { BarkReportData } from "@/lib/bark/bark-models";
-import { PoolClient } from "pg";
-import {
-  dbBegin,
-  dbCommit,
-  dbRelease,
-  dbResultQuery,
-  dbRollback,
-} from "@/lib/data/db-utils";
-import { SQL_QUERY, loadSql } from "../_sql/load-sql";
-import { EncryptionService } from "@/lib/services/encryption";
+import { dbBegin, dbCommit, dbRelease, dbRollback } from "@/lib/data/db-utils";
+import { selectPendingAppointmentExists } from "../_queries/select-pending-appointment-exists";
+import { insertReport } from "../_queries/insert-report";
+import { updateAppointment } from "../_queries/update-appointment";
+import { CALL_OUTCOME } from "@/lib/data/db-enums";
 
 export async function createReport(
   config: PgBarkServiceConfig,
@@ -27,109 +22,34 @@ export async function createReport(
 > {
   const { dbPool, textEncryptionService } = config;
   const { appointmentId, reportData } = args;
+  const { ineligibilityReason, ...otherFields } = reportData;
   const conn = await dbPool.connect();
-  const ctx: _Context = {
-    conn,
-    textEncryptionService,
-    appointmentId,
-    reportData,
-  };
   try {
+    const encryptedIneligibilityReason =
+      await textEncryptionService.getEncryptedData(ineligibilityReason);
     await dbBegin(conn);
-    const checkResult = await checkAppointmentExists(ctx);
-    if (checkResult !== CODE.OK) {
-      return Err(checkResult);
+    const { exists } = await selectPendingAppointmentExists(conn, {
+      appointmentId,
+    });
+    if (!exists) {
+      return Err(CODE.ERROR_APPOINTMENT_NOT_FOUND);
     }
-    const { result, error } = await insertReportAndUpdateCall(ctx);
-    if (error !== undefined) {
-      return Err(error);
-    }
+    const { reportId } = await insertReport(conn, {
+      appointmentId,
+      encryptedIneligibilityReason,
+      ...otherFields,
+    });
+    await updateAppointment(conn, {
+      appointmentId,
+      appointmentStatus: CALL_OUTCOME.REPORTED,
+    });
     await dbCommit(conn);
-    return Ok(result);
-  } catch {
+    return Ok({ reportId });
+  } catch (e) {
+    console.error(e);
     await dbRollback(conn);
     return Err(CODE.STORAGE_FAILURE);
   } finally {
     await dbRelease(conn);
   }
-}
-
-type _Context = {
-  conn: PoolClient;
-  textEncryptionService: EncryptionService;
-  appointmentId: string;
-  reportData: BarkReportData;
-};
-
-async function checkAppointmentExists(
-  ctx: _Context,
-): Promise<
-  | typeof CODE.OK
-  | typeof CODE.ERROR_APPOINTMENT_NOT_FOUND
-  | typeof CODE.STORAGE_FAILURE
-> {
-  const { conn, appointmentId } = ctx;
-  const sql = loadSql(SQL_QUERY.SELECT_APPOINTMENT_EXISTS);
-  const { result, error } = await dbResultQuery<{ appointmentExists: boolean }>(
-    conn,
-    sql,
-    [appointmentId],
-  );
-  if (error === CODE.DB_QUERY_FAILURE) {
-    return CODE.STORAGE_FAILURE;
-  }
-  if (result.rows.length !== 1) {
-    return CODE.STORAGE_FAILURE;
-  }
-  const { appointmentExists } = result.rows[0];
-  if (!appointmentExists) {
-    return CODE.ERROR_APPOINTMENT_NOT_FOUND;
-  }
-  return CODE.OK;
-}
-
-async function insertReportAndUpdateCall(
-  ctx: _Context,
-): Promise<Result<{ reportId: string }, typeof CODE.STORAGE_FAILURE>> {
-  const { conn, textEncryptionService, appointmentId, reportData } = ctx;
-  const {
-    visitTime,
-    dogWeightKg,
-    dogBodyConditioningScore,
-    dogDidDonateBlood,
-    dogHeartworm,
-    dogDea1Point1,
-    ineligibilityStatus,
-    ineligibilityReason,
-    ineligibilityExpiryTime,
-  } = reportData;
-  const encryptedIneligibilityReason =
-    ineligibilityReason === ""
-      ? ""
-      : await textEncryptionService.getEncryptedData(ineligibilityReason);
-  const sql = loadSql(SQL_QUERY.INSERT_REPORT_UPDATE_CALL);
-  const { result, error } = await dbResultQuery<{ reportId: string }>(
-    conn,
-    sql,
-    [
-      appointmentId,
-      visitTime,
-      dogWeightKg,
-      dogBodyConditioningScore,
-      dogDidDonateBlood,
-      dogHeartworm,
-      dogDea1Point1,
-      ineligibilityStatus,
-      encryptedIneligibilityReason,
-      ineligibilityExpiryTime,
-    ],
-  );
-  if (error === CODE.DB_QUERY_FAILURE) {
-    return Err(CODE.STORAGE_FAILURE);
-  }
-  if (result.rows.length !== 1) {
-    return Err(CODE.STORAGE_FAILURE);
-  }
-  const { reportId } = result.rows[0];
-  return Ok({ reportId });
 }
