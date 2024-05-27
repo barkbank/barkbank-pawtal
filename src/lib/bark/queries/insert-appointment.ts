7626d4325f41f714f7774e1399ce4a55739c9bbe
Jin <hsienjin@gmail.com>
2024-05-27 22:07:23 +0800

    clean-whitespace - remove trailing whitespace

--- src/lib/bark/queries/insert-appointment.ts
+++ src/lib/bark/queries/insert-appointment.ts
@@ -13,7 +13,7 @@ export async function insertAppointment(
   const sql = `
   INSERT INTO calls (dog_id, vet_id, call_outcome, encrypted_opt_out_reason)
   VALUES ($1, $2, 'APPOINTMENT', '')
-  RETURNING call_id::text as "appointmentId"  
+  RETURNING call_id::text as "appointmentId"
   `;
   const res = await dbQuery<{ appointmentId: string }>(dbContext, sql, [
     dogId,
