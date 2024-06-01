c56bf24c53a05358deb13c8ea24d6907b10928ab
Jin <hsienjin@gmail.com>
2024-06-01 09:06:27 +0800

    impl-submit-report - stub opFetchAppointment

---
+++ src/lib/bark/operations/op-fetch-appointment.ts
@@ -0,0 +1,16 @@
+import { Err, Result } from "@/lib/utilities/result";
+import { BarkContext } from "../bark-context";
+import { BarkAppointment } from "../bark-models";
+import { CODE } from "@/lib/utilities/bark-code";
+
+export async function opFetchAppointment(
+  context: BarkContext,
+  args: { appointmentId: string },
+): Promise<
+  Result<
+    BarkAppointment,
+    typeof CODE.FAILED | typeof CODE.ERROR_APPOINTMENT_NOT_FOUND
+  >
+> {
+  return Err(CODE.FAILED);
+}
