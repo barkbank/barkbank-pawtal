4362f19bba46cf52da936bd58946581afa66ea89
Jin <hsienjin@gmail.com>
2024-06-01 10:58:58 +0800

    impl-submit-report - impl postBarkReportData server action

--- src/app/vet/(logged-in)/appointments/_actions/post-bark-report-data.ts
+++ src/app/vet/(logged-in)/appointments/_actions/post-bark-report-data.ts
@@ -1,17 +1,34 @@
 "use server";
 
+import APP from "@/lib/app";
 import { getAuthenticatedVetActor } from "@/lib/auth";
 import { BarkReportData } from "@/lib/bark/bark-models";
+import { opSubmitReport } from "@/lib/bark/operations/op-submit-report";
 import { CODE } from "@/lib/utilities/bark-code";
-import { Err } from "@/lib/utilities/result";
+import { Err, Result } from "@/lib/utilities/result";
 
 export async function postBarkReportData(args: {
   appointmentId: string;
   reportData: BarkReportData;
-}) {
+}): Promise<
+  Result<
+    { reportId: string },
+    | typeof CODE.ERROR_NOT_LOGGED_IN
+    | typeof CODE.ERROR_APPOINTMENT_NOT_FOUND
+    | typeof CODE.UNAUTHORIZED
+    | typeof CODE.FAILED
+  >
+> {
   const actor = await getAuthenticatedVetActor();
   if (actor === null) {
     return Err(CODE.ERROR_NOT_LOGGED_IN);
   }
-  const { reportData } = args;
+  const { vetId: actorVetId } = actor.getParams();
+  const { appointmentId, reportData } = args;
+  const context = await APP.getBarkContext();
+  return opSubmitReport(context, {
+    appointmentId,
+    reportData,
+    actorVetId,
+  });
 }
