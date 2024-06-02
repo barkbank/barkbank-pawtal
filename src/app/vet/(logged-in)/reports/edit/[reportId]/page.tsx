1dc14b795e120f34348e008259a76d061b1c1264
Jin <hsienjin@gmail.com>
2024-06-02 22:54:28 +0800

    list-reports - retrieve and present reports and JSON in vet/reports/list

--- src/app/vet/(logged-in)/reports/edit/[reportId]/page.tsx
+++ src/app/vet/(logged-in)/reports/edit/[reportId]/page.tsx
@@ -2,11 +2,11 @@ import { getAuthenticatedVetActor } from "@/lib/auth";
 import { RoutePath } from "@/lib/route-path";
 import { redirect } from "next/navigation";
 
-export default async function Page(props: {params: {reportId: string}}) {
+export default async function Page(props: { params: { reportId: string } }) {
   const actor = await getAuthenticatedVetActor();
   if (actor === null) {
     redirect(RoutePath.VET_LOGIN_PAGE);
   }
-  const {reportId} = props.params;
+  const { reportId } = props.params;
   return <div>TODO: Stub Page</div>;
 }
--- src/app/vet/(logged-in)/reports/list/page.tsx
+++ src/app/vet/(logged-in)/reports/list/page.tsx
@@ -1,4 +1,7 @@
+import { BarkError } from "@/components/bark/bark-error";
+import APP from "@/lib/app";
 import { getAuthenticatedVetActor } from "@/lib/auth";
+import { opFetchReportsByVetId } from "@/lib/bark/operations/op-fetch-reports-by-vet-id";
 import { RoutePath } from "@/lib/route-path";
 import { redirect } from "next/navigation";
 
@@ -7,5 +10,16 @@ export default async function Page() {
   if (actor === null) {
     redirect(RoutePath.VET_LOGIN_PAGE);
   }
-  return <div>TODO: Stub Page</div>;
+  const context = await APP.getBarkContext();
+  const vetId = actor.getVetId();
+  const { result, error } = await opFetchReportsByVetId(context, { vetId });
+  if (error !== undefined) {
+    return <BarkError className="m-3">Failed to fetch reports.</BarkError>;
+  }
+  const { reports } = result;
+  return (
+    <div className="m-3">
+      TODO: Stub Page <pre>{JSON.stringify(reports, null, 2)}</pre>
+    </div>
+  );
 }
