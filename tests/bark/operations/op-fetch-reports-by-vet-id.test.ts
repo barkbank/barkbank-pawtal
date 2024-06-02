4fa95a5f8601a32ad4920440c27dfcf1de7b2be6
Jin <hsienjin@gmail.com>
2024-06-02 15:02:25 +0800

    op-fetch-reports-by-vet-id - stub tests for opFetchReportsByVetId

---
+++ tests/bark/operations/op-fetch-reports-by-vet-id.test.ts
@@ -0,0 +1,10 @@
+import { withBarkContext } from "../_context";
+
+describe("opFetchReportsByVetId", () => {
+  it("WIP: should return empty list when vet has no reports", async () => {
+    await withBarkContext(async ({ context }) => {});
+  });
+  it("WIP: should return reports", async () => {
+    await withBarkContext(async ({ context }) => {});
+  });
+});
