943c9c89d96c8e968ae3f9152b5e72f39bb3eb90
Jin <hsienjin@gmail.com>
2024-06-02 08:18:54 +0800

    op-cancel-appointment - stub tests

---
+++ tests/bark/operations/op-cancel-appointment.test.ts
@@ -0,0 +1,13 @@
+import { withBarkContext } from "../_context"
+
+describe("opCancelAppointment", () => {
+  it("WIP: should return ERROR_APPOINTMENT_NOT_FOUND when the appointment does not exist", async () => {
+    await withBarkContext(async ({context}) => {})
+  })
+  it("WIP: should return ERROR_NOT_ALLOWED when the actorVetId is not the vet of the appointment", async () => {
+    await withBarkContext(async ({context}) => {})
+  })
+  it("WIP: should cancel the appointment", async () => {
+    await withBarkContext(async ({context}) => {})
+  })
+})
