fe2ee2a21bad9486063266e4be3dbacf465070f2
Jin <hsienjin@gmail.com>
2024-06-02 10:29:25 +0800

    zod-queries - EncryptedBarkReportDataSchema

--- src/lib/bark/models/encrypted-bark-report-data.ts
+++ src/lib/bark/models/encrypted-bark-report-data.ts
@@ -1,26 +1,14 @@
-import {
-  PosNegNilSchema,
-  ReportedIneligibilitySchema,
-} from "@/lib/data/db-enums";
 import { z } from "zod";
+import { BarkReportDataSchema } from "./bark-report-data";
 
 /**
  * The encrypted form of BarkReportData replaces ineligibilityReason with
  * encryptedIneligibilityReason.
  */
-export const EncryptedBarkReportDataSchema = z.object({
-  visitTime: z.date(),
-  dogWeightKg: z.number(),
-
-  // BCS score range is 1-9 https://vcahospitals.com/know-your-pet/body-condition-scores
-  dogBodyConditioningScore: z.number().min(1).max(9),
-
-  dogHeartworm: PosNegNilSchema,
-  dogDea1Point1: PosNegNilSchema,
-  ineligibilityStatus: ReportedIneligibilitySchema,
+export const EncryptedBarkReportDataSchema = BarkReportDataSchema.omit({
+  ineligibilityReason: true,
+}).extend({
   encryptedIneligibilityReason: z.string(),
-  ineligibilityExpiryTime: z.date().nullable(),
-  dogDidDonateBlood: z.boolean(),
 });
 
 export type EncryptedBarkReportData = z.infer<
