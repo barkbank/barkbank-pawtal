42bc7513697a2458122e26832b8ad1ff4b8e8263
Jin <hsienjin@gmail.com>
2024-06-02 22:45:16 +0800

    list-reports - redirect vet/reports to vet/reports/list

--- src/app/vet/(logged-in)/reports/page.tsx
+++ src/app/vet/(logged-in)/reports/page.tsx
@@ -1,5 +1,8 @@
+import { redirect } from "next/navigation";
 import { ReportsExplorer } from "./_components/reports-explorer";
+import { RoutePath } from "@/lib/route-path";
 
 export default async function Page() {
+  redirect(RoutePath.VET_REPORTS_LIST);
   return <ReportsExplorer />;
 }
