be372b002680292e677907bbdd03affd5fe92fb9
Jin <hsienjin@gmail.com>
2024-06-03 09:21:46 +0800

    list-reports - add a todo comment

--- src/lib/bark/models/dog-id-info.ts
+++ src/lib/bark/models/dog-id-info.ts
@@ -1,12 +1,13 @@
 import { z } from "zod";
 import { DogGenderSchema } from "./dog-gender";
 
+// TODO: Add preferredVetName - Useful when presented to Users.
+
 /**
  * Dog Identifiable Information (DII)
  *
  * These are a set of natural values that can be used to identify a dog.
  */
-
 export const DogIdInfoSchema = z.object({
   dogName: z.string(),
   dogGender: DogGenderSchema,
