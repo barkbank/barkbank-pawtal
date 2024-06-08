4c2b58f2a686358651cf4004ee83817e7624eb6a
Jin <hsienjin@gmail.com>
2024-06-08 10:28:50 +0800

    view-report - rename to VetAppointmentCancelPage

--- e2e/_lib/pom/pages/vet-appointment-cancel-page.ts
+++ e2e/_lib/pom/pages/vet-appointment-cancel-page.ts
@@ -2,7 +2,7 @@ import { RoutePath } from "@/lib/route-path";
 import { PomDynamicPage } from "../core/pom-dynamic-page";
 import { Locator } from "@playwright/test";
 
-export class VetCancelAppointmentPage extends PomDynamicPage {
+export class VetAppointmentCancelPage extends PomDynamicPage {
   urlRegex(): RegExp {
     return RoutePath.VET_APPOINTMENTS_CANCEL_REGEX;
   }
--- e2e/vet/vet-can-cancel-appointment.spec.ts
+++ e2e/vet/vet-can-cancel-appointment.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { initPomContext } from "../_lib/init/init-pom-context";
 import { doCreateAppointment } from "../_lib/sequences/do-create-appointment";
 import { VetAppointmentListPage } from "../_lib/pom/pages/vet-appointment-list-page";
-import { VetCancelAppointmentPage } from "../_lib/pom/pages/vet-cancel-appointment-page";
+import { VetAppointmentCancelPage } from "../_lib/pom/pages/vet-appointment-cancel-page";
 
 test("vet can cancel appointment", async ({ page }) => {
   const context = await initPomContext({ page });
@@ -13,7 +13,7 @@ test("vet can cancel appointment", async ({ page }) => {
   await expect(pg1.appointmentCard({ dogName }).locator()).toBeVisible();
   await pg1.appointmentCard({ dogName }).cancelAppointmentButton().click();
 
-  const pg2 = new VetCancelAppointmentPage(context);
+  const pg2 = new VetAppointmentCancelPage(context);
   await pg2.checkUrl();
   await pg2.confirmButton().click();
 
