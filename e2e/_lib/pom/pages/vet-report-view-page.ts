f3c538b718e5fd295506db84a32d6e3be25f4fc7
Jin <hsienjin@gmail.com>
2024-06-08 08:56:56 +0800

    view-report - define pom VetReportViewPage

---
+++ e2e/_lib/pom/pages/vet-report-view-page.ts
@@ -0,0 +1,17 @@
+import { RoutePath } from "@/lib/route-path";
+import { PomDynamicPage } from "../core/pom-dynamic-page";
+import { Locator } from "@playwright/test";
+
+export class VetReportViewPage extends PomDynamicPage {
+  urlRegex(): RegExp {
+    return RoutePath.VET_REPORTS_VIEW_REGEX;
+  }
+
+  backButton(): Locator {
+    return this.page().getByRole("link", {name: "Back", exact: true});
+  }
+
+  editButton(): Locator {
+    return this.page().getByRole("link", {name: "Edit", exact: true});
+  }
+}
