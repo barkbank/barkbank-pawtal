84bcf8d7fe5f3575d145019f1d22ae1dda007fb8
Jin <hsienjin@gmail.com>
2024-06-09 11:47:32 +0800

    durations - check that duration is not falsy

--- src/lib/utilities/add-duration.ts
+++ src/lib/utilities/add-duration.ts
@@ -11,12 +11,15 @@ import {
 
 export function addDuration(reference: Date, duration: Duration): Date {
   let result = reference;
-  result = addYears(result, duration?.years ?? 0);
-  result = addMonths(result, duration?.months ?? 0);
-  result = addWeeks(result, duration?.weeks ?? 0);
-  result = addDays(result, duration?.days ?? 0);
-  result = addHours(result, duration?.hours ?? 0);
-  result = addMinutes(result, duration?.minutes ?? 0);
-  result = addSeconds(result, duration?.seconds ?? 0);
+  if (!duration) {
+    return result;
+  }
+  result = addYears(result, duration.years ?? 0);
+  result = addMonths(result, duration.months ?? 0);
+  result = addWeeks(result, duration.weeks ?? 0);
+  result = addDays(result, duration.days ?? 0);
+  result = addHours(result, duration.hours ?? 0);
+  result = addMinutes(result, duration.minutes ?? 0);
+  result = addSeconds(result, duration.seconds ?? 0);
   return result;
 }
