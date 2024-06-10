1e6e6fe66e7a3c96626050093ddbed0a18163647
Jin <hsienjin@gmail.com>
2024-06-11 00:43:24 +0800

    use-latest-values - call-task - partially defined

---
+++ src/lib/bark/models/call-task.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { SpecifiedDogGenderSchema } from "./dog-gender";
+
+export const CallTaskSchema = z.object({
+  dogId: z.string(),
+  dogName: z.string(),
+  dogBreed: z.string(),
+  dogBirthday: z.date(),
+  dogGender: SpecifiedDogGenderSchema,
+  dogWeightKg: z.number().optional(),
+  // WIP: dogEverReceivedTransfusion
+  // WIP: dogEverPregnant
+  // WIP: ownerName
+})
+
+
+// export type xAvailableDog = {
+//   dogId: string;
+//   dogName: string;
+//   dogBreed: string;
+//   dogBirthday: Date;
+//   dogGender: DogGender;
+//   dogWeightKg: number | null;
+//   dogEverReceivedTransfusion: YesNoUnknown;
+//   dogEverPregnant: YesNoUnknown;
+// };
