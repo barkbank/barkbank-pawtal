84a4aad1fa93e81ee25d30f180664467d0d7189a
Jin <hsienjin@gmail.com>
2024-06-11 00:01:29 +0800

    use-latest-values - stub out the test cases

--- tests/bark/operations/op-fetch-call-tasks-by-vet-id.test.ts
+++ tests/bark/operations/op-fetch-call-tasks-by-vet-id.test.ts
@@ -1,3 +1,19 @@
+import { withBarkContext } from "../_context";
+
 describe("opFetchCallTasksByVetId", () => {
-  // WIP: Stub out the test scenarios. See get-available-dogs.test.ts for inspiration.
+  it("WIP: should return empty list when vet has no applicable call tasks", async () => {
+    await withBarkContext(async ({ context }) => {});
+  });
+  it("WIP: should return list of call tasks", async () => {
+    await withBarkContext(async ({ context }) => {});
+  });
+  it("WIP: should use latest values in call task", async () => {
+    await withBarkContext(async ({ context }) => {});
+  });
+  it("WIP: should exclude dog profile that is incomplete", async () => {
+    await withBarkContext(async ({ context }) => {});
+  });
+  it("WIP: should exclude call task of another vet", async () => {
+    await withBarkContext(async ({ context }) => {});
+  });
 });
