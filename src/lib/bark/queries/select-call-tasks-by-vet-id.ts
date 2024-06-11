de9269f54528fc38c4c4fac8ef2ab1118e20ad0d
Jin <hsienjin@gmail.com>
2024-06-12 00:03:57 +0800

    use-latest-values - partial query

--- src/lib/bark/queries/select-call-tasks-by-vet-id.ts
+++ src/lib/bark/queries/select-call-tasks-by-vet-id.ts
@@ -1,10 +1,37 @@
-import { DbContext } from "@/lib/data/db-utils";
-import { EncryptedCallTask } from "../models/encrypted-call-task";
+import { DbContext, dbQuery } from "@/lib/data/db-utils";
+import {
+  EncryptedCallTask,
+  EncryptedCallTaskSchema,
+} from "../models/encrypted-call-task";
 
+/**
+ * Call tasks are dogs that can be scheduled.
+ *
+ * - the actor is a preferred vet of the dog
+ * - AND dog has no pending reports
+ * - AND service_status is AVAILABLE
+ * - AND profile_status is COMPLETE
+ * - AND medical_status is ELIGIBLE
+ * - AND participation_status is PARTICPATING
+ *
+ * @returns list of encrypted call tasks
+ */
 export async function selectCallTasksByVetId(
   dbContext: DbContext,
   args: { vetId: string },
 ): Promise<EncryptedCallTask[]> {
-  // WIP: write the sql query
-  return [];
+  const { vetId } = args;
+  const sql = `
+  SELECT
+  FROM dog_vet_preferences as tPref
+  LEFT JOIN dog_statuses as tStatus on tPref.dog_id = tStatus.dog_id
+  LEFT JOIN calls as tCall as tPref.dog_id = tCall.dog_id
+  WHERE tPref.vet_id = $1
+  AND tStatus.service_status = 'AVAILABLE'
+  AND tStatus.profile_status = 'COMPLETE'
+  AND tStatus.medical_status = 'ELIGIBLE'
+  AND tStatus.participation_status = 'PARTICIPATING'
+  `;
+  const res = await dbQuery<EncryptedCallTask>(dbContext, sql, [vetId]);
+  return res.rows.map((row) => EncryptedCallTaskSchema.parse(row));
 }
