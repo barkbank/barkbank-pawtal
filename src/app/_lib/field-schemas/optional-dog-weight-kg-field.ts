56a59097b9f7a9540312f373073d834ea31a0dfc
Jin <hsienjin@gmail.com>
2024-06-15 18:28:03 +0800

    field-schemas - impl OptionalDogWeightKgField

--- src/app/_lib/field-schemas/optional-dog-weight-kg-field.ts
+++ src/app/_lib/field-schemas/optional-dog-weight-kg-field.ts
@@ -5,15 +5,15 @@ import {
 } from "@/lib/utilities/bark-utils";
 import { z } from "zod";
 
-export class RequiredDogWeightKgField {
+export class OptionalDogWeightKgField {
   static new() {
-    return new RequiredDogWeightKgField();
+    return new OptionalDogWeightKgField();
   }
   schema() {
     return z.string().refine(
       (value: string) => {
         if (isBlank(value)) {
-          return false;
+          return true;
         }
         return isValidWeightKg(value);
       },
@@ -23,7 +23,10 @@ export class RequiredDogWeightKgField {
     );
   }
 
-  parse(value: string): number {
+  parse(value: string): number | null {
+    if (isBlank(value)) {
+      return null;
+    }
     return parseWeightKg(value)!;
   }
 }
