6d6e36c938962968e1c781a845b789cf10bfc7fa
Jin <hsienjin@gmail.com>
2024-06-16 16:32:15 +0800

    Update FAQs (#201)
    Update the questions and answers to follow this list -
    https://www.notion.so/Pawtal-FAQ-3f4c0e5792154ecda16043f7dd92d60c

--- src/app/user/(logged-in)/info/page.tsx
+++ src/app/user/(logged-in)/info/page.tsx
@@ -10,6 +10,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Link from "next/link";
 
 const criterias = [
   { text: ">20KG in weight", imgUrl: IMG_PATH.WEIGHING_MACHINE },
@@ -60,60 +61,199 @@ const registrationSteps = [
   },
 ];
 
+const ContactEmail = () => (
+  <Link href="mailto:hello@barkbank.co">hello@barkbank.co</Link>
+);
+const PrivacyPolicy = () => (
+  <Link href="https://www.barkbank.co/privacy-policy">
+    https://www.barkbank.co/privacy-policy
+  </Link>
+);
+
 const faqs = [
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed augue lacus viverra vitae congue eu. Quam nulla porttitor massa id neque. Laoreet non curabitur gravida arcu ac. Eget duis at tellus at urna condimentum mattis. Cursus mattis molestie a iaculis. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Amet massa vitae tortor condimentum lacinia quis vel eros. Feugiat nisl pretium fusce id velit ut. Diam vel quam elementum pulvinar etiam non quam lacus. Etiam non quam lacus suspendisse faucibus. Tempus imperdiet nulla malesuada pellentesque.",
+    question: `What does it mean if my dog is labeled as "Permanently Ineligible" for blood donation?`,
+    answer: (
+      <p>
+        Your dog is considered permanently ineligible if they have ever been
+        pregnant, received a blood transfusion, are over 8 years old, or have a
+        medical report indicating permanent ineligibility.
+      </p>
+    ),
   },
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Dogs can donate blood every 3 months. However, the frequency of donation is subject to the discretion of the attending veterinarian.",
+    question: `What does "Unknown" eligibility status signify?`,
+    answer: (
+      <p>
+        The &ldquo;Unknown&rdquo; status means that it is not confirmed whether
+        your dog is permanently ineligible due to unknown pregnancy or blood
+        transfusion history, or unspecified breed and weight.
+      </p>
+    ),
   },
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Dogs can donate blood every 3 months. However, the frequency of donation is subject to the discretion of the attending veterinarian.",
+    question: `Under what conditions is my dog "Temporarily Ineligible" for donating blood?`,
+    answer: (
+      <p>
+        Your dog is temporarily ineligible if they are underweight (less than 20
+        KG), underage (less than 1 year), recently donated blood (within the
+        last 3 months), recently vaccinated (within the last 2 weeks), recently
+        tested positive for heartworm (within the last 6 months), or have been
+        deemed temporarily ineligible by a vet.
+      </p>
+    ),
   },
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Dogs can donate blood every 3 months. However, the frequency of donation is subject to the discretion of the attending veterinarian.",
+    question: `How is a dog determined to be "Eligible" for blood donation?`,
+    answer: (
+      <>
+        <p>
+          A dog is eligible when they meet all the criteria for blood donation,
+          have completed all required medical information and currently in
+          active participation. Criteria for canine blood donation:
+        </p>
+        <p>
+          <b>Weight:</b> Dogs should weigh at least 25kg but not be overweight.
+        </p>
+        <p>
+          <b>Age:</b> Dogs between 1 to 8 years old are generally eligible.
+        </p>
+        <p>
+          <b>Temperament:</b> Donor dogs should have a good and calm temperament
+          to minimise stress during the process.
+        </p>
+        <p>
+          <b>Health:</b> They should be healthy with no underlying health
+          conditions.
+        </p>
+        <p>
+          <b>Blood Type:</b> Dogs must have DEA 1.1 or 1.2 negative blood type.
+        </p>
+        <p>
+          <b>Vaccinations:</b> Donor dogs should be up to date on vaccinations.
+        </p>
+        <p>
+          <b>Parasite Prevention:</b> They should be on regular parasite
+          prevention medication for heart worm, tick, and flea control.
+        </p>
+        <p>
+          <b>Blood Transfusion:</b> Dogs that have received a blood transfusion
+          are not eligible to donate.
+        </p>
+        <p>
+          <b>Donation Frequency:</b> A healthy dog can donate blood every 3
+          months, which equals up to 4 times a year.
+        </p>
+        <p>
+          <b>Gender:</b> Male dogs or nulliparous (never pregnant) female dogs
+          are typically accepted.
+        </p>
+        <p>
+          <b>Medications:</b> Donor dogs should not be on any medications that
+          could pose a problem for the recipient.
+        </p>
+      </>
+    ),
   },
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Dogs can donate blood every 3 months. However, the frequency of donation is subject to the discretion of the attending veterinarian.",
+    question: `What is the Bark Bank blood donation process?`,
+    answer: (
+      <>
+        <p>
+          <b>Step 1 — Registration:</b> Begin by registering your dog on Bark
+          Bank&apos;s Canine Blood Registry Pawtal, completing your contact
+          information, providing basic medical details about your dog, and
+          selecting your preferred vet donation point.
+        </p>
+        <p>
+          <b>Step 2 — Appointment scheduling:</b> Once registered, your vet will
+          schedule an appointment for health screening, blood profiling test,
+          and blood donation.
+        </p>
+        <p>
+          <b>Step 3 — Vet appointment:</b> Head to your scheduled vet
+          appointment with your dog at the designated time for a health check
+          and blood donation
+        </p>
+        <p>
+          <b>Step 4 — Medical Report:</b> Your vet will update your dog&apos;s
+          medical details directly on the Pawtal after the appointment.
+        </p>
+      </>
+    ),
   },
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Dogs can donate blood every 3 months. However, the frequency of donation is subject to the discretion of the attending veterinarian.",
+    question: `Can I contact the vet directly?`,
+    answer: (
+      <p>
+        Yes, you may arrange an appointment with the vet directly. However, it
+        is encouraged to register with Bark Bank so that dogs in need have a
+        centralised system with updated donor dog details. This helps ensure
+        that all necessary information is available and organised, facilitating
+        better coordination and care.
+      </p>
+    ),
   },
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Dogs can donate blood every 3 months. However, the frequency of donation is subject to the discretion of the attending veterinarian.",
+    question: `What happens if I need to reschedule or cancel my vet appointment?`,
+    answer: (
+      <p>
+        Please contact the vet directly to reschedule or cancel your
+        appointment. They will be able to assist you with finding a new time
+        that works for both you and the veterinary office.
+      </p>
+    ),
   },
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Dogs can donate blood every 3 months. However, the frequency of donation is subject to the discretion of the attending veterinarian.",
+    question: `What if my dog is unwell after an appointment has been scheduled?`,
+    answer: (
+      <p>
+        If your dog is unwell after scheduling an appointment, it is important
+        to either reschedule the appointment with the vet or change the
+        participation status if the dog will be unavailable for more than 2
+        weeks. This ensures that your dog receives the necessary care and rest,
+        and the donation schedule is updated accordingly to reflect their
+        current health status.
+      </p>
+    ),
   },
   {
-    question: "How often can my dog donate blood?",
-    questionSubtext: "Things on a very small scale behave like nothing",
-    answer:
-      "Dogs can donate blood every 3 months. However, the frequency of donation is subject to the discretion of the attending veterinarian.",
+    question: `Who should I reach out to for additional questions?`,
+    answer: (
+      <p>
+        Please email <ContactEmail /> if you have more questions. We are here to
+        assist you with any inquiries you may have.
+      </p>
+    ),
+  },
+  {
+    question: `How can I delete my account?`,
+    answer: (
+      <p>
+        You can write in to <ContactEmail /> to request for your account to be
+        deleted. We will guide you through the process and address any concerns
+        you may have.
+      </p>
+    ),
+  },
+  {
+    question: `What is the Bark Bank privacy policy?`,
+    answer: (
+      <p>
+        The Bark Bank privacy policy details how we handle your data and protect
+        your privacy. You can review our policy at <PrivacyPolicy />.
+      </p>
+    ),
+  },
+  {
+    question: `How can I request for my data?`,
+    answer: (
+      <p>
+        To request all the information collected about you and understand how it
+        has been used, please write to <ContactEmail />. We will provide you
+        with the necessary details and assist with any further queries.
+      </p>
+    ),
   },
 ];
 
@@ -184,31 +324,25 @@ export default async function Page() {
           ))}
         </article>
       </section>
-      <section className="mx-[60px] my-[80px] flex max-w-7xl flex-col items-center gap-[50px] ">
+      <section className="mx-[60px] my-[80px] flex max-w-7xl flex-col items-center gap-[50px]">
         <div className="mb-[21px] text-center sm:col-span-2 md:col-span-3 lg:mt-[40px]">
           <BarkH2>Frequently Asked Questions</BarkH2>
         </div>
-        <div className="grid grid-cols-1 gap-[30px] sm:grid-cols-2 md:grid-cols-3">
-          {faqs.map(({ question, questionSubtext, answer }) => (
+        <div className="grid grid-cols-1 gap-[30px] sm:grid-cols-2 xl:grid-cols-3">
+          {faqs.map(({ question, answer }, idx) => (
             <Accordion
-              key={question}
+              key={idx}
               orientation="vertical"
               type="single"
               collapsible
-              className="p-[25px] sm:max-w-[333px]"
             >
-              <AccordionItem
-                className="border-none"
-                key={question}
-                value={answer}
-              >
+              <AccordionItem className="border-none" value={`item-${idx}`}>
                 <AccordionTrigger>
-                  <div className="flex flex-col gap-y-1 text-start">
-                    <p className="font-bold">{question}</p>
-                    <p className="font-thin text-[grey]">{questionSubtext}</p>
-                  </div>
+                  <p className="w-full text-left text-base font-semibold">
+                    {question}
+                  </p>
                 </AccordionTrigger>
-                <AccordionContent>{answer}</AccordionContent>
+                <AccordionContent className="prose">{answer}</AccordionContent>
               </AccordionItem>
             </Accordion>
           ))}
