2fe8e746a8d080d497f32b4e77b144b4ab94eba2
Jin <hsienjin@gmail.com>
2024-06-16 11:00:42 +0800

    remove-date-picker - update getTestBirthday in e2e-test-utils to return d-mmm-yyyy dates in SG timezone

--- e2e/_lib/e2e-test-utils.ts
+++ e2e/_lib/e2e-test-utils.ts
@@ -1,15 +1,17 @@
 import { sprintf } from "sprintf-js";
 import { PomContext } from "./pom/core/pom-object";
 import { HeaderComponent } from "./pom/layout/header-component";
+import { SGT_UI_DATE, SINGAPORE_TIME_ZONE, formatDateTime, parseCommonDate } from "@/lib/utilities/bark-time";
 
 export function getTestBirthday(ageYears: number): string {
-  // WIP: Change this to return 18 Aug 2022 style dates. So we know where all the validation needs to update.
   const ts = new Date();
   const y = ts.getUTCFullYear() - ageYears;
   const m = ts.getUTCMonth() + 1;
   const d = ts.getUTCDate();
   const ymd = sprintf("%04d-%02d-%02d", y, m, d);
-  return ymd;
+  const birthdayDate = parseCommonDate(ymd, SINGAPORE_TIME_ZONE);
+  const birthdayString = formatDateTime(birthdayDate, SGT_UI_DATE);
+  return birthdayString;
 }
 
 export async function getIsMobile(context: PomContext): Promise<boolean> {
