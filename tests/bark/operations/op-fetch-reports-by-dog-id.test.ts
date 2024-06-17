04283c0cb53d39f740436984b4cbd67bc7a99809
Jin <hsienjin@gmail.com>
2024-06-17 14:40:42 +0800

    impl-op-fetch-reports-by-dog-id - write tests

--- tests/bark/operations/op-fetch-reports-by-dog-id.test.ts
+++ tests/bark/operations/op-fetch-reports-by-dog-id.test.ts
@@ -1,10 +1,88 @@
-import { withBarkContext } from "../_context"
+import { opFetchReportsByDogId } from "@/lib/bark/operations/op-fetch-reports-by-dog-id";
+import { withBarkContext } from "../_context";
+import { givenDog, givenVet } from "../_given";
+import { BarkContext } from "@/lib/bark/bark-context";
+import { opRecordAppointmentCallOutcome } from "@/lib/bark/operations/op-record-appointment-call-outcome";
+import { opSubmitReport } from "@/lib/bark/operations/op-submit-report";
+import { mockReportData } from "../_mocks";
+import {
+  SINGAPORE_TIME_ZONE,
+  parseCommonDate,
+} from "@/lib/utilities/bark-time";
 
 describe("opFetchReportsByDogId", () => {
-  it("WIP: returns empty list when dog has no reports", async () => {
-    await withBarkContext(async ({context}) => {})
-  })
-  it("WIP: returns reports, newest to oldest", async () => {
-    await withBarkContext(async ({context}) => {})
-  })
-})
+  it("returns empty list when dog has no reports", async () => {
+    await withBarkContext(async ({ context }) => {
+      const d1 = await givenDog(context);
+      const { result, error } = await opFetchReportsByDogId(context, {
+        dogId: d1.dogId,
+      });
+      expect(error).toBeUndefined();
+      expect(result).toEqual({ reports: [] });
+    });
+  });
+  it("returns reports, newest to oldest", async () => {
+    await withBarkContext(async ({ context }) => {
+      // Given vet v1
+      const { vetId } = await givenVet(context, { vetIdx: 1 });
+
+      // And dog d1, with preferred vet v1
+      const { dogId } = await givenDog(context, {
+        dogIdx: 1,
+        preferredVetId: vetId,
+      });
+
+      // And d1 has three reports from v1
+      const r1 = await addReport(context, {
+        dogId,
+        vetId,
+        visitTime: toVisitTime("10 May 2023"), // Middle
+      });
+      const r2 = await addReport(context, {
+        dogId,
+        vetId,
+        visitTime: toVisitTime("10 Jan 2023"), // Oldest
+      });
+      const r3 = await addReport(context, {
+        dogId,
+        vetId,
+        visitTime: toVisitTime("10 Aug 2023"), // Newest
+      });
+
+      // When...
+      const { result, error } = await opFetchReportsByDogId(context, { dogId });
+
+      // Then...
+      expect(error).toBeUndefined();
+      const reports = result!.reports;
+      expect(reports[0].reportId).toEqual(r3.reportId);
+      expect(reports[1].reportId).toEqual(r1.reportId);
+      expect(reports[2].reportId).toEqual(r2.reportId);
+    });
+  });
+});
+
+function toVisitTime(value: string): Date {
+  return parseCommonDate(value, SINGAPORE_TIME_ZONE);
+}
+
+async function addReport(
+  context: BarkContext,
+  args: { dogId: string; vetId: string; visitTime: Date },
+): Promise<{ appointmentId: string; reportId: string }> {
+  const { dogId, vetId, vetId: actorVetId, visitTime } = args;
+  const reportData = {
+    ...mockReportData(),
+    visitTime,
+  };
+  const res1 = await opRecordAppointmentCallOutcome(context, { dogId, vetId });
+  const appointmentId = res1.result!.appointmentId;
+
+  const res2 = await opSubmitReport(context, {
+    appointmentId,
+    reportData,
+    actorVetId,
+  });
+  const reportId = res2.result!.reportId;
+  return { appointmentId, reportId };
+}
