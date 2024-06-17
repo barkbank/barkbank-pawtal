8ec2a11b5aad36a679f1625937442b8c7b9f2a5b
Jin <hsienjin@gmail.com>
2024-06-17 08:41:54 +0800

    user-can-list-reports - update the plan to include moving models from dog-models into bark/models so that we can have proper zod schemas for the types.

--- src/app/user/(logged-in)/my-pets/view-dog/[dogId]/page.tsx
+++ src/app/user/(logged-in)/my-pets/view-dog/[dogId]/page.tsx
@@ -194,7 +194,7 @@ export default async function Page(props: { params: { dogId: string } }) {
   const dogAgeMonths = getAgeMonths(dogBirthday, new Date());
 
   // WIP: === REFACTORING STEPS ===
-  // WIP: Create pull request to continually test the UI as we refactor.
+  // *WIP: Move dog-models into bark/models with Zod schemas.
   // WIP: Extract the following into a DogViewer component inside page.tsx first.
   // WIP: Then move DogViewer into _lib/components/dog-viewer.tsx with all the view components and functions.
   // WIP: Extract from DogViewer components DogStatus and DogProfile
