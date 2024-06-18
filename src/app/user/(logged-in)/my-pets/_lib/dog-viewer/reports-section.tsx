f3d9437b2702fb401f29705dbccc4ef366b4b848
Jin <hsienjin@gmail.com>
2024-06-18 09:02:04 +0800

    dog-viewer - exporatory presentation of reports in prose.

--- src/app/user/(logged-in)/my-pets/_lib/dog-viewer/reports-section.tsx
+++ src/app/user/(logged-in)/my-pets/_lib/dog-viewer/reports-section.tsx
@@ -1,14 +1,58 @@
 import { Separator } from "@/components/ui/separator";
 import { DogViewerData } from "./dog-viewer-data";
+import { BarkReport } from "@/lib/bark/models/bark-report";
+import { SGT_UI_DATE, formatDateTime } from "@/lib/utilities/bark-time";
+import { POS_NEG_NIL } from "@/lib/bark/enums/pos-neg-nil";
 
 export function ReportsSection(props: { data: DogViewerData }) {
   const { dogReports } = props.data;
-
   return (
     <div className="x-card flex flex-col gap-3">
       <p className="x-card-title">Reports</p>
       <Separator />
-      <pre>{JSON.stringify(dogReports, null, 2)}</pre>
+      {dogReports.map((report) => {
+        const { reportId } = report;
+        return <_Report key={reportId} report={report} />;
+      })}
+    </div>
+  );
+}
+
+// WIP: Need to add vet details to the report. (Remember, reports can come from past preferred vets which may be different from the current.)
+export function _Report(props: { report: BarkReport }) {
+  const { report } = props;
+  const {
+    visitTime,
+    vetId,
+    dogName,
+    dogWeightKg,
+    dogHeartworm,
+    dogBodyConditioningScore,
+  } = report;
+  const dateText = formatDateTime(visitTime, SGT_UI_DATE);
+
+  return (
+    <div>
+      <div className="prose">
+        <h4>
+          {dateText} report from vet {vetId}.
+        </h4>
+        <p>
+          {dogName} was presented on {dateText}, weighing in at {dogWeightKg}{" "}
+          kilograms. Overall, its body conditioning score (BCS) was{" "}
+          {dogBodyConditioningScore}.
+        </p>
+        {dogHeartworm === POS_NEG_NIL.POSITIVE && (
+          <p>
+            {dogName} tested positive for heartworms and is therefore ineligible
+            for blood donation.
+          </p>
+        )}
+        {dogHeartworm === POS_NEG_NIL.NEGATIVE && (
+          <p>{dogName} tested negative for heartworms.</p>
+        )}
+      </div>
+      {/* <pre>{JSON.stringify(report, null, 2)}</pre> */}
     </div>
   );
 }
