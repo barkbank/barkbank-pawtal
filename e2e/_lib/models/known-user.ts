b6e1fb454eed8a093f4356832d5cfb7d84ab50ea
Jin <hsienjin@gmail.com>
2024-06-22 15:48:00 +0800

    test-user-can-view-report - fmt

--- e2e/_lib/models/known-user.ts
+++ e2e/_lib/models/known-user.ts
@@ -5,11 +5,13 @@ export const KnownUserSchema = z.object({
   userEmail: z.string(),
   userPhoneNumber: z.string(),
   userResidency: z.string(),
-  userDogs: z.array(z.object({
-    dogName: z.string(),
-    dogStatus: z.string(),
-    vetName: z.string().optional(),
-  }))
+  userDogs: z.array(
+    z.object({
+      dogName: z.string(),
+      dogStatus: z.string(),
+      vetName: z.string().optional(),
+    }),
+  ),
 });
 
 export type KnownUser = z.infer<typeof KnownUserSchema>;
