2b8fe26b2bc924147a3ef8191cc3744f138771ab
Jin <hsienjin@gmail.com>
2024-06-22 15:47:44 +0800

    test-user-can-view-report - define known-user model

---
+++ e2e/_lib/models/known-user.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const KnownUserSchema = z.object({
+  userName: z.string(),
+  userEmail: z.string(),
+  userPhoneNumber: z.string(),
+  userResidency: z.string(),
+  userDogs: z.array(z.object({
+    dogName: z.string(),
+    dogStatus: z.string(),
+    vetName: z.string().optional(),
+  }))
+});
+
+export type KnownUser = z.infer<typeof KnownUserSchema>;
