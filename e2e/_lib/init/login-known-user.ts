ebb566701ac9d61774666039ebd19d236a6c8bec
Jin <hsienjin@gmail.com>
2024-06-22 15:50:14 +0800

    test-user-can-view-report - move to get-known-user

--- e2e/_lib/init/login-known-user.ts
+++ e2e/_lib/init/login-known-user.ts
@@ -1,6 +1,6 @@
 import { Page } from "@playwright/test";
 import { PomContext } from "../pom/core/pom-object";
-import { getKnownUser } from "../known-entities/known-user";
+import { getKnownUser } from "../utils/get-known-user";
 import { PomUser } from "../pom/entities";
 import { UserMyPetsPage } from "../pom/pages/user-my-pets-page";
 import { doUserLoginSequence } from "../ops/do-user-login-sequence";
