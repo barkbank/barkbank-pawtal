82099faf3d13ff44f1635518fcdbf7368adc9131
Jin <hsienjin@gmail.com>
2024-06-23 08:16:31 +0800

    edit-sub-profile - pass props into SubProfileForm

--- src/app/user/(logged-in)/my-pets/_lib/components/sub-profile-form-controller.tsx
+++ src/app/user/(logged-in)/my-pets/_lib/components/sub-profile-form-controller.tsx
@@ -1,11 +1,36 @@
 import { BarkFormOption } from "@/components/bark/bark-form";
 import { SubProfileForm } from "./sub-profile-form";
 import { DogProfile } from "@/lib/bark/models/dog-profile";
+import { SubProfile } from "@/lib/bark/models/sub-profile";
+import { Ok, Result } from "@/lib/utilities/result";
 
 export function SubProfileFormController(props: {
   vetOptions: BarkFormOption[];
   dogId: string;
   existingDogProfile: DogProfile;
 }) {
-  return <SubProfileForm />;
+  const { vetOptions, dogId, existingDogProfile } = props;
+
+  const onCancel = async () => {
+    console.debug({ _msg: "onCancel was triggered" });
+  };
+
+  const onSubmit = async (
+    subProfile: SubProfile,
+  ): Promise<Result<true, string>> => {
+    console.debug({
+      _msg: "onSubmit was triggered",
+      subProfile,
+    });
+    return Ok(true);
+  };
+
+  return (
+    <SubProfileForm
+      vetOptions={vetOptions}
+      dogProfile={existingDogProfile}
+      handleCancel={onCancel}
+      handleSubmit={onSubmit}
+    />
+  );
 }
--- src/app/user/(logged-in)/my-pets/_lib/components/sub-profile-form.tsx
+++ src/app/user/(logged-in)/my-pets/_lib/components/sub-profile-form.tsx
@@ -1,3 +1,13 @@
-export function SubProfileForm() {
+import { BarkFormOption } from "@/components/bark/bark-form";
+import { DogProfile } from "@/lib/bark/models/dog-profile";
+import { SubProfile } from "@/lib/bark/models/sub-profile";
+import { Result } from "@/lib/utilities/result";
+
+export function SubProfileForm(props: {
+  vetOptions: BarkFormOption[];
+  dogProfile: DogProfile;
+  handleSubmit: (subProfile: SubProfile) => Promise<Result<true, string>>;
+  handleCancel: () => Promise<void>;
+}) {
   return <div>SubProfileForm</div>;
 }
