9fb0e9c25a693a95a3a58ee5e2bb573c25e91d26
Jin <hsienjin@gmail.com>
2024-06-23 20:56:02 +0800

    BARK-2053 - Update logout component to use BarkButton

--- src/app/logout/_components.tsx
+++ src/app/logout/_components.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { BarkButton } from "@/components/bark/bark-button";
 import { BarkH2, BarkP } from "@/components/bark/bark-typography";
-import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
@@ -19,20 +19,22 @@ export function LogoutQuestion() {
         <BarkH2>Logout</BarkH2>
         <BarkP>Are you sure you want to logout?</BarkP>
         <div className="mt-6 flex flex-col gap-3 md:flex-row">
-          <Button
+          <BarkButton
             className="w-full md:w-1/2"
             variant="brandInverse"
             onClick={onCancel}
+            type="button"
           >
             Cancel
-          </Button>
-          <Button
+          </BarkButton>
+          <BarkButton
             className="w-full md:w-1/2"
             variant="brand"
             onClick={onLogout}
+            type="button"
           >
             Logout
-          </Button>
+          </BarkButton>
         </div>
       </div>
     </div>
