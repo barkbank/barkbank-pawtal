44f90b7b6d2229114e0fcdd7fc4675f1454f9b03
Jin <hsienjin@gmail.com>
2024-06-30 14:11:22 +0800

    BARK-6617 - Comment

--- scripts/get-branch.sh
+++ scripts/get-branch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Generate a BARK-? branch name
+
 branchName="BARK-$((RANDOM % 10000))"
 while [ -n "$(git log --oneline --all | grep $branchName)" ]; do
     branchName="BARK-$((RANDOM % 10000))"
