d8ac9003f32b11e57f6b380cdf3a5015c18bdafb
Jin <hsienjin@gmail.com>
2024-06-30 14:10:51 +0800

    BARK-6617 - Implement get-branch.sh

--- scripts/get-branch.sh
+++ scripts/get-branch.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "BARK-$[$RANDOM % 10000]"
+branchName="BARK-$((RANDOM % 10000))"
+while [ -n "$(git log --oneline --all | grep $branchName)" ]; do
+    branchName="BARK-$((RANDOM % 10000))"
+done
+echo $branchName
