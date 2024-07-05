3ee8e49410153f4b30073f6dcce9b5b47462e820
Jin <hsienjin@gmail.com>
2024-07-05 22:48:35 +0800

    BARK-5778: Update make-ticket.sh

--- scripts/make-ticket.sh
+++ scripts/make-ticket.sh
@@ -2,8 +2,8 @@
 
 # Generate a BARK-? branch name
 
-branchName="BARK-$((RANDOM % 10000))"
+branchName=$(printf "BARK-%04d" $((RANDOM % 10000)))
 while [ -n "$(git log --oneline --all | grep $branchName)" ]; do
-    branchName="BARK-$((RANDOM % 10000))"
+    branchName=$(printf "BARK-%04d" $((RANDOM % 10000)))
 done
 echo $branchName
