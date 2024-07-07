b8a416755ad6ae0c8083da99f11ff6060eb26725
Jin <hsienjin@gmail.com>
2024-07-07 23:32:18 +0800

    BARK-18: Remove dog_breeds.json

--- src/app/examples/autocomplete/page.tsx
+++ src/app/examples/autocomplete/page.tsx
@@ -1,23 +1,8 @@
-import dog_breeds_json from "@/resources/dog_breeds.json";
-import { z } from "zod";
 import { BreedForm } from "./_lib/breed-form";
-
-const DogBreedJsonSchema = z.object({
-  dog_breeds: z.array(
-    z.object({
-      dog_breed: z.string(),
-      wikipedia_url: z.string(),
-    }),
-  ),
-});
-
-function getBreeds(): string[] {
-  const data = DogBreedJsonSchema.parse(dog_breeds_json);
-  return data.dog_breeds.map((item) => item.dog_breed);
-}
+import { getDogBreeds } from "@/app/_lib/get-dog-breeds";
 
 export default async function Page() {
-  const breeds = getBreeds();
+  const breeds = getDogBreeds();
   return (
     <div className="x-card m-3">
       <BreedForm breeds={breeds} />
