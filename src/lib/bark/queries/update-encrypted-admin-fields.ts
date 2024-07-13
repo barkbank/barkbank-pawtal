f85896eab5066bf6aa3449a3706df135f30cf512
Jin <hsienjin@gmail.com>
2024-07-13 14:20:46 +0800

    BARK-13: Impl updateEncryptedAdminFields

---
+++ src/lib/bark/queries/update-encrypted-admin-fields.ts
@@ -0,0 +1,20 @@
+import { DbContext, dbQuery } from "@/lib/data/db-utils";
+import { EncryptedAdminFields } from "../models/encrypted-admin-fields";
+
+export async function updateEncryptedAdminFields(
+  dbContext: DbContext,
+  args: {
+    encryptedAdminFields: EncryptedAdminFields;
+  },
+): Promise<void> {
+  const { adminId, adminEncryptedPii } = args.encryptedAdminFields;
+  const sql = `
+  UPDATE admins
+  SET admin_encrypted_pii = $2
+  WHERE admin_id = $1
+  `;
+  const res = await dbQuery(dbContext, sql, [adminId, adminEncryptedPii]);
+  if (res.rows.length !== 1) {
+    throw new Error(`Failed to update admin record (adminId=${adminId})`);
+  }
+}
