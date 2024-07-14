ec9a77862baa5f9ca54bf8f2f5aebe3010722a73
Jin <hsienjin@gmail.com>
2024-07-14 10:07:34 +0800

    BARK-42: UI Test - vet can login using alternative vet account

---
+++ e2e/_lib/utils/get-known-vet-account.ts
@@ -0,0 +1,7 @@
+import { KnownVetAccount } from "../models/known-vet-account";
+
+export function getKnownVetAccount(): KnownVetAccount {
+  return {
+    vetAccountEmail: "manager1@vet.com",
+  };
+}
