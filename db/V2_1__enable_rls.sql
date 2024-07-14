0aafd4bbe31d269df194bc9c35eaf6ef77877b63
Jin <hsienjin@gmail.com>
2024-07-14 10:26:18 +0800

    BARK-42: Define RLS for vet_accounts table

--- db/V2_1__enable_rls.sql
+++ db/V2_1__enable_rls.sql
@@ -5,6 +5,7 @@ ALTER TABLE dogs ENABLE ROW LEVEL SECURITY;
 ALTER TABLE reports ENABLE ROW LEVEL SECURITY;
 ALTER TABLE users ENABLE ROW LEVEL SECURITY;
 ALTER TABLE vets ENABLE ROW LEVEL SECURITY;
+ALTER TABLE vet_accounts ENABLE ROW LEVEL SECURITY;
 
 -- It is not possible for flyway to enable RLS on flyway_schema_history while it
 -- is using the table. You'll have to do this manually...
