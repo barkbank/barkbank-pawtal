4d36c19b214ed068f6e9ca187608ce1fa37ebf0a
Jin <hsienjin@gmail.com>
2024-07-18 20:26:46 +0800

    BARK-21: Stub toIneligibilityReasons

---
+++ src/lib/bark/mappers/to-ineligibility-reasons.ts
@@ -0,0 +1,9 @@
+import { IneligibilityReason } from "../enums/ineligibility-reason";
+import { IneligibilityFactors } from "../models/ineligibility-factors";
+
+export function toIneligibilityReasons(
+  factors: IneligibilityFactors,
+): IneligibilityReason[] {
+  // WIP: Impl toIneligibilityReasons
+  return [];
+}
