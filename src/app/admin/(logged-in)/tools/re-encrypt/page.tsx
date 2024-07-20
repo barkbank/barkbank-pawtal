6b614342a254b5fdca589d6e93be4f2afb84ea5d
Jin <hsienjin@gmail.com>
2024-07-20 08:40:27 +0800

    BARK-64: Use default button in admin re-encrypt tool

--- src/app/admin/(logged-in)/tools/re-encrypt/page.tsx
+++ src/app/admin/(logged-in)/tools/re-encrypt/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { postReEncrypt } from "@/app/admin/_lib/actions/post-re-encrypt";
-import { BarkButton } from "@/components/bark/bark-button";
 import { BarkError } from "@/components/bark/bark-error";
+import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import { ReEncryptResult } from "@/lib/bark/models/re-encrypt-result";
 import { useState } from "react";
@@ -33,14 +33,14 @@ export default function Page() {
         Use the Re-enrypt button below to re-enrypt all data with the latest
         key.
       </p>
-      <BarkButton
-        variant="brand"
-        className="w-full md:w-40"
+      <Button
+        variant="default"
+        className="w-full p-6 md:w-40"
         type="button"
         onClick={reEncrypt}
       >
         Re-encrypt
-      </BarkButton>
+      </Button>
       <p>Re-encryption Result</p>
       <pre>{JSON.stringify(result, null, 2)}</pre>
       {error && <BarkError>{error}</BarkError>}
