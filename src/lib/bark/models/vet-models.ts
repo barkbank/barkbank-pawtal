e938879e0b337a238466788322474190f4d95e36
Jin <hsienjin@gmail.com>
2024-07-20 14:24:27 +0800

    BARK-27: Define spec schemas in vet-models

--- src/lib/bark/models/vet-models.ts
+++ src/lib/bark/models/vet-models.ts
@@ -1,27 +1,34 @@
 import { z } from "zod";
 
-export const SecureVetAccountSchema = z.object({
+export const VetAccountIdSchema = z.object({ vetAccountId: z.string() });
+
+export const SecureVetAccountSpecSchema = z.object({
   vetId: z.string(),
-  vetAccountId: z.string(),
   vetAccountHashedEmail: z.string(),
   vetAccountEncryptedName: z.string(),
   vetAccountEncryptedEmail: z.string(),
 });
 
-export const VetAccountSchema = z.object({
+export const SecureVetAccountSchema = VetAccountIdSchema.merge(
+  SecureVetAccountSpecSchema,
+);
+
+export const VetAccountSpecSchema = z.object({
   vetId: z.string(),
-  vetAccountId: z.string(),
   vetAccountEmail: z.string().email(),
   vetAccountName: z.string(),
 });
 
-export const VetClinicSchema = z.object({
-  vetId: z.string(),
+export const VetAccountSchema = VetAccountIdSchema.merge(VetAccountSpecSchema);
+
+export const VetIdSchema = z.object({ vetId: z.string() });
+export const VetClinicSpecSchema = z.object({
   vetEmail: z.string().email(),
   vetName: z.string(),
   vetPhoneNumber: z.string(),
   vetAddress: z.string(),
 });
+export const VetClinicSchema = VetIdSchema.merge(VetClinicSpecSchema);
 
 export const VetProfileSchema = z.object({
   clinic: VetClinicSchema,
@@ -37,7 +44,10 @@ export const VetLoginSchema = z.object({
 });
 
 export type SecureVetAccount = z.infer<typeof SecureVetAccountSchema>;
+export type SecureVetAccountSpec = z.infer<typeof SecureVetAccountSpecSchema>;
 export type VetAccount = z.infer<typeof VetAccountSchema>;
+export type VetAccountSpec = z.infer<typeof VetAccountSpecSchema>;
+export type VetClinicSpec = z.infer<typeof VetClinicSpecSchema>;
 export type VetClinic = z.infer<typeof VetClinicSchema>;
 export type VetProfile = z.infer<typeof VetProfileSchema>;
 export type VetLogin = z.infer<typeof VetLoginSchema>;
