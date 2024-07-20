e91db63d429b45d8e50c87135f01e1eacfc71d0b
Jin <hsienjin@gmail.com>
2024-07-20 21:31:40 +0800

    BARK-28: Impl opCreateVetClinic

---
+++ src/lib/bark/operations/op-create-vet-clinic.ts
@@ -0,0 +1,21 @@
+import { Err, Ok, Result } from "@/lib/utilities/result";
+import { VetClinic, VetClinicSpec } from "../models/vet-models";
+import { BarkContext } from "../bark-context";
+import { CODE } from "@/lib/utilities/bark-code";
+import { VetClinicDao } from "../queries/vet-clinic-dao";
+
+export async function opCreateVetClinic(
+  context: BarkContext,
+  args: { spec: VetClinicSpec },
+): Promise<Result<{ clinic: VetClinic }, typeof CODE.FAILED>> {
+  const { dbPool } = context;
+  const { spec } = args;
+  try {
+    const dao = new VetClinicDao(dbPool);
+    const clinic = await dao.insert({ spec });
+    return Ok({ clinic });
+  } catch (err) {
+    console.error(err);
+    return Err(CODE.FAILED);
+  }
+}
