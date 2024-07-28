024e8384c4b5fd6b7d2bd88c7ba0e9001a36cb26
Jin <hsienjin@gmail.com>
2024-07-28 21:57:42 +0800

    BARK-29: Implement a simple queue

---
+++ src/lib/utilities/queue.ts
@@ -0,0 +1,34 @@
+export class Queue<T> {
+  private head: _Item<T> | undefined;
+  private tail: _Item<T> | undefined;
+
+  offer(value: T) {
+    const item = new _Item(value);
+    if (this.tail === undefined) {
+      this.head = item;
+      this.tail = item;
+    } else {
+      this.tail.next = item;
+      this.tail = item;
+    }
+  }
+
+  poll(): T | undefined {
+    if (this.head === undefined) {
+      return undefined;
+    }
+    const item = this.head;
+    this.head = item.next;
+    if (this.head === undefined) {
+      this.tail = undefined;
+    }
+    return item.value;
+  }
+}
+
+class _Item<T> {
+  constructor(
+    public value: T,
+    public next?: _Item<T> | undefined,
+  ) {}
+}
