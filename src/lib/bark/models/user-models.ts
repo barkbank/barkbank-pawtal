c8ae4079513827a83fbba7ed99e61b588f97cee0
Jin <hsienjin@gmail.com>
2024-08-10 16:59:18 +0800

    BARK-113: Define MyAccountSchema

--- src/lib/bark/models/user-models.ts
+++ src/lib/bark/models/user-models.ts
@@ -1,17 +1,19 @@
-import { UserResidency } from "../enums/user-residency";
+import { UserResidency, UserResidencySchema } from "../enums/user-residency";
 import { DogGender } from "../enums/dog-gender";
 import { DogAppointment } from "./dog-appointment";
 import { DogStatuses } from "./dog-statuses";
+import { z } from "zod";
 
-// WIP: Define MyAccountSchema
-export type MyAccount = {
-  userCreationTime: Date;
-  // WIP: Add userTitle
-  userName: string;
-  userResidency: UserResidency;
-  userEmail: string;
-  userPhoneNumber: string;
-};
+export const MyAccountSchema = z.object({
+  userCreationTime: z.date(),
+  userTitle: z.string().optional(),
+  userName: z.string(),
+  userResidency: UserResidencySchema,
+  userEmail: z.string().email(),
+  userPhoneNumber: z.string(),
+});
+
+export type MyAccount = z.infer<typeof MyAccountSchema>;
 
 export type MyLastContactedTime = {
   userLastContactedTime: Date | null;
