7d5833dd1ddd8f606f93b2622ae76a20c3118356
Jin <hsienjin@gmail.com>
2024-08-17 13:24:07 +0800

    BARK-113: Remove userTitle from generated user

--- e2e/_lib/models/generated-user.ts
+++ e2e/_lib/models/generated-user.ts
@@ -1,8 +1,6 @@
-import { UserTitleSchema } from "@/lib/bark/enums/user-title";
 import { z } from "zod";
 
 export const GeneratedUserSchema = z.object({
-  userTitle: UserTitleSchema,
   userName: z.string(),
   userEmail: z.string(),
   userPhoneNumber: z.string(),
