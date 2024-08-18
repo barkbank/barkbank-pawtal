218b364259b416e3a55d8f6eac534abf84a7d38d
Jin <hsienjin@gmail.com>
2024-08-18 21:24:50 +0800

    BARK-125: Put tracker effect behind a suspense boundary so that it can access useSearchParams

--- src/app/_components/tracker.tsx
+++ src/app/_components/tracker.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { postOnPageLoad } from "../_lib/actions/post-on-page-load";
 import { ClientInfo } from "@/lib/bark/models/tracker-models";
 
 export function Tracker() {
+  return (
+    <Suspense fallback={<_Fallback />}>
+      <_Effect />
+    </Suspense>
+  );
+}
+
+function _Fallback() {
+  return null;
+}
+
+function _Effect() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const queryString = searchParams.toString();
@@ -15,6 +27,6 @@ export function Tracker() {
       queryString,
     };
     postOnPageLoad({ clientInfo });
-  }, [pathname]);
+  }, [pathname, queryString]);
   return null;
 }
