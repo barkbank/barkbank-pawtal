f8ee2040fc442a92a86521b1316a129c5cae281b
Jin <hsienjin@gmail.com>
2024-08-21 22:55:27 +0800

    BARK-235: Change default result to 'No Results'

--- src/app/admin/(logged-in)/tools/webflow-importer/page.tsx
+++ src/app/admin/(logged-in)/tools/webflow-importer/page.tsx
@@ -40,7 +40,7 @@ const RegistrationDataSchema = z.object({
 type RegistrationData = z.infer<typeof RegistrationDataSchema>;
 
 export default function Page() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState("No Results");
   const form = useForm<FormData>({
     resolver: zodResolver(FormDataSchema),
     defaultValues: {
