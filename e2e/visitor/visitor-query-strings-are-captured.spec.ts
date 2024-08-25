4bcce9436f02ccb7ea38a821c88f2c331255625d
Jin <hsienjin@gmail.com>
2024-08-25 15:47:52 +0800

    BARK-249: Use UUID for nonce and wait a little before checking DB

--- e2e/visitor/visitor-query-strings-are-captured.spec.ts
+++ e2e/visitor/visitor-query-strings-are-captured.spec.ts
@@ -1,16 +1,19 @@
 import test, { expect } from "@playwright/test";
 import { initPomContext } from "../_lib/init/init-pom-context";
-import { toString } from "lodash";
 import { RoutePath } from "@/lib/route-path";
 import { ApiClient } from "../_lib/pom/api/api-client";
+import { v4 } from "uuid";
 
 test("query strings are captured", async ({ page, request }) => {
   const context = await initPomContext({ page });
-  const nonce = toString(Math.floor(Math.random() * 1000000000));
+  const nonce = v4();
   const queryString = `nonce=${nonce}`;
   const path = `${RoutePath.USER_LOGIN_PAGE}?${queryString}`;
   const url = context.website.urlOf(path);
   await page.goto(url);
+
+  // Wait some time for DB to be written
+  await page.waitForTimeout(500);
 
   const api = new ApiClient(context, request);
   const sql = `
