06b4ac7619efbfc53efaeaa0b28edd38ceb9ca3b
Jin <hsienjin@gmail.com>
2024-08-25 22:57:06 +0800

    BARK-120: Define donor_snapshots table

--- db/V2__ref_tables.sql
+++ db/V2__ref_tables.sql
@@ -151,3 +151,18 @@ CREATE TABLE pawtal_events (
 );
 
 CREATE INDEX idx_pawtal_events_event_ts ON pawtal_events(event_ts);
+
+CREATE TABLE donor_snapshots (
+  "snapshot_id" BIGSERIAL,
+  "day" DATE NOT NULL,
+  "dog_id" BIGINT NOT NULL,
+  "dog_weight_kg" REAL NULL,
+  "dog_age_months" INTEGER NOT NULL,
+  "dog_dea1_point1" t_pos_neg_nil NOT NULL,
+  "dog_medical_status" t_medical_status NOT NULL,
+  "dog_profile_status" t_profile_status NOT NULL,
+  "user_id" BIGINT NOT NULL,
+  "user_residency" t_residency NOT NULL,
+  CONSTRAINT unique_day_dog UNIQUE ("day", "dog_id"),
+  CONSTRAINT donor_snapshots_pk PRIMARY KEY ("snapshot_id")
+)
