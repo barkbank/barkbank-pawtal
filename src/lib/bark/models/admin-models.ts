39cf33f42a1e363f985505b42dc4b21b76077cb3
Jin <hsienjin@gmail.com>
2024-08-28 21:30:55 +0800

    BARK-79: Define admin models

--- src/lib/bark/models/admin-models.ts
+++ src/lib/bark/models/admin-models.ts
@@ -1,9 +1,32 @@
 import { z } from "zod";
 
+export const AdminAccountSchema = z.object({
+  adminId: z.string(),
+  adminEmail: z.string().email(),
+  adminName: z.string(),
+  adminPhoneNumber: z.string(),
+  adminCanManageAdminAccounts: z.boolean(),
+  adminCanManageVetAccounts: z.boolean(),
+  adminCanManageUserAccounts: z.boolean(),
+  adminCanManageDonors: z.boolean(),
+});
+
 export const AdminPiiSchema = z.object({
   adminEmail: z.string().email(),
   adminName: z.string(),
   adminPhoneNumber: z.string(),
 });
 
+export const EncryptedAdminAccountSchema = z.object({
+  adminId: z.string(),
+  adminHashedEmail: z.string().email(),
+  adminEncryptedPii: z.string(),
+  adminCanManageAdminAccounts: z.boolean(),
+  adminCanManageVetAccounts: z.boolean(),
+  adminCanManageUserAccounts: z.boolean(),
+  adminCanManageDonors: z.boolean(),
+});
+
+export type AdminAccount = z.infer<typeof AdminAccountSchema>;
 export type AdminPii = z.infer<typeof AdminPiiSchema>;
+export type EncryptedAdminAccount = z.infer<typeof EncryptedAdminAccountSchema>;
