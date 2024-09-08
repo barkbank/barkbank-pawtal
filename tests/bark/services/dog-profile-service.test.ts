bfd884bcc89d32fe6897b2f01321b1be3ea9f988
Jin <hsienjin@gmail.com>
2024-09-08 17:06:34 +0800

    BARK-87: Test can be used to create and retrieve dog profile

--- tests/bark/services/dog-profile-service.test.ts
+++ tests/bark/services/dog-profile-service.test.ts
@@ -4,10 +4,32 @@ import { UserAccountSpec } from "@/lib/bark/models/user-models";
 import { USER_TITLE } from "@/lib/bark/enums/user-title";
 import { USER_RESIDENCY } from "@/lib/bark/enums/user-residency";
 import { UserAccountService } from "@/lib/bark/services/user-account-service";
+import {
+  DogProfile,
+  DogProfileSchema,
+} from "@/lib/bark/models/dog-profile-models";
+import { dateAgo } from "../../_time_helpers";
+import { DOG_ANTIGEN_PRESENCE } from "@/lib/bark/enums/dog-antigen-presence";
+import { YES_NO_UNKNOWN } from "@/lib/bark/enums/yes-no-unknown";
+import { DOG_GENDER } from "@/lib/bark/enums/dog-gender";
+import { DogProfileService } from "@/lib/bark/services/dog-profile-service";
+import { VetClinicSpec } from "@/lib/bark/models/vet-models";
+import { VetAccountService } from "@/lib/bark/services/vet-account-service";
 
 describe("DogProfileService", () => {
   it("can be used to create and retrieve dog profile", async () => {
-    await withBarkContext(async ({ context }) => {});
+    await withBarkContext(async ({ context }) => {
+      const { userId } = await _createTestUser({ context, idx: 1 });
+      const { vetId } = await _createTestVetClinic({ context, idx: 2 });
+      const dogProfile = _mockDogProfile({ dogPreferredVetId: vetId });
+      const service = new DogProfileService({ context });
+      const res1 = await service.addDogProfile({ userId, dogProfile });
+      const { dogId } = res1.result!;
+      const res2 = await service.getDogProfile({ userId, dogId });
+      const retrieved = res2.result!;
+      expect(retrieved).toMatchObject(dogProfile);
+      expect(dogProfile).toMatchObject(retrieved);
+    });
   });
   it("can list dog profiles by user", async () => {
     await withBarkContext(async ({ context }) => {});
@@ -32,6 +54,22 @@ describe("DogProfileService", () => {
   });
 });
 
+function _mockDogProfile(overrides?: Partial<DogProfile>): DogProfile {
+  const base: DogProfile = {
+    dogName: "Woofgang",
+    dogBirthday: dateAgo({ numYears: 3 }),
+    dogBreed: "German Guard Dog",
+    dogGender: DOG_GENDER.MALE,
+    dogWeightKg: 26.5,
+    dogDea1Point1: DOG_ANTIGEN_PRESENCE.UNKNOWN,
+    dogEverPregnant: YES_NO_UNKNOWN.NO,
+    dogEverReceivedTransfusion: YES_NO_UNKNOWN.NO,
+    dogPreferredVetId: "",
+  };
+  const out = { ...base, ...overrides };
+  return DogProfileSchema.parse(out);
+}
+
 async function _createTestUser(args: {
   context: BarkContext;
   idx: number;
@@ -51,4 +89,24 @@ async function _createTestUser(args: {
   }
   const { userId } = result;
   return { userId };
+}
+
+async function _createTestVetClinic(args: {
+  context: BarkContext;
+  idx: number;
+}): Promise<{ vetId: string }> {
+  const { context, idx } = args;
+  const spec: VetClinicSpec = {
+    vetName: `Vet ${idx}`,
+    vetEmail: `admin@vet${idx}.com`,
+    vetPhoneNumber: `1800 123 ${100 + idx}`,
+    vetAddress: `${1000 + idx} Longest Street, Vetri, SG 169819`,
+  };
+  const service = new VetAccountService({ context });
+  const res = await service.createVetClinic({ spec });
+  if (res.error !== undefined) {
+    throw new Error("Failed to create clinic");
+  }
+  const { vetId } = res.result.clinic;
+  return { vetId };
 }
