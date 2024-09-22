1119a64b88af9aa73e51502564482db9dc6ef471
Jin <hsienjin@gmail.com>
2024-09-22 08:34:11 +0800

    BARK-51: Update make-ticket.sh to use a sliding window

--- scripts/make-ticket.sh
+++ scripts/make-ticket.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Config
+TICKET_PREFIX="BARK-"
+
 # Generate a random branch within the specified limits.
 #
 # Branch name would be BARK-X where offset <= X < offset+limit.
@@ -12,17 +15,17 @@ function getRandomBranchName {
     local offset=$1
     local limit=$2
     local ticketNumber=$(($offset + (RANDOM % $limit)))
-    echo "BARK-${ticketNumber}"
+    echo "${TICKET_PREFIX}${ticketNumber}"
 }
 
 # Find a branch name that isn't already on the Git Log
 function getBranchName {
-    local offset=1
-    local limit=8
+    local logs=$(git log --oneline --all | grep ${TICKET_PREFIX})
+    local offset=0
+    local limit=50
     local branchName=$(getRandomBranchName $offset $limit)
-    while [ -n "$(git log --oneline --all | grep $branchName\\b)" ]; do
-        # offset=$(($offset + $limit))
-        limit=$(($limit * 2))
+    while [ -n "$(echo ${logs} | grep $branchName\\b)" ]; do
+	offset=$(($offset + 10))
         branchName=$(getRandomBranchName $offset $limit)
     done
     echo $branchName
@@ -34,6 +37,9 @@ function main {
 
     echo "Choosing a ticket name..."
     local branchName=$(getBranchName)
+
+    echo "Selected ticket..."
+    echo ${branchName}
 
     echo "Claiming the ticket..."
     git checkout -b ${branchName} origin/main
